9ffa7b4f8cb0caa8c286d78d656040266f69423b
David Herrera <davidfherrerar@gmail.com>
2015-12-16 10:48:38 -0500

    Adding all the code for the app, webview/mobile, and the backend scripts for the admin panel and the listener

--- AdminPanel-MySQLDB-NodeListener-Docs/clearFirebaseRequests.js
+++
@@ -1,5 +0,0 @@
-var Firebase    =require('firebase');
-
-var Ref = new Firebase ( 'https://luminous-heat-8715.firebaseio.com/requests');
-
-Ref.set({request1 : { name : 'john' , request : 'appointment'} });
