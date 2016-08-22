27fd5bd31a835dd942be3fa9caf6229b43490636
David Herrera <davidfherrerar@gmail.com>
2016-08-22 18:11:39 -0400

    Comments to all services with ngdocs, more translation, fix to treatment status page, fix to password reset functionality, request sent to backend on resume and every two minutes, fixing of accountSettings

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var gulpDocs = require('gulp-ngdocs');
+var connect = require('gulp-connect');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 //var karma = require('gulp-karma');
 gulp.task('lint', function() {
     return gulp.src(['www/js/**/*.js', './test/**/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
+
 
 gulp.task('watch-lint', function() {
     gulp.watch(['www/**/*'], ['lint']);
@@ -37,8 +42,37 @@ gulp.task('copyToCordovaFolder',function()
 {
     gulp.src('./www/**/*').pipe(gulp.dest('/Users/davidherrera/Documents/Projects/muhc/www'));
 });
+gulp.task('minconcatjs',function()
+{
+    gulp.src('./www/js/filters/*').pipe(concat('filters.js')).pipe(uglify()).pipe(gulp.dest('/Users/davidherrera/Documents/Projects/muhc/www/js'));
+});
+gulp.task('ngdocs', [], function () {
+    var options = {
+    html5Mode: true,
+    scripts: [
+      'http://ajax.googleapis.com/ajax/libs/angularjs/1.5.1/angular.min.js',
+      'http://ajax.googleapis.com/ajax/libs/angularjs/1.5.1/angular-animate.min.js'
+    ],
+    startPage: '/api/MUHCApp',
+    title: "Opal Mobile App",
+    titleLink: "/api/MUHCApp"
+  };
+  return gulp.src(['www/js/services/*.js', 'www/js/app.js'])
+    .pipe(gulpDocs.process(options))
+    .pipe(gulp.dest('./docs'));
+});
+gulp.task('watch-docs',function(){
+     gulp.watch(['www/js/**/*.js'],['ngdocs']);
+});
+gulp.task('connect_ngdocs', function() {
 
-
+  connect.server({
+    root: 'docs',
+    livereload: false,
+    fallback: 'docs/index.html',
+    port: 8083
+  });
+});
 gulp.task('watch',function()
 {
     gulp.watch(['www/**/*'],['copyToCordovaFolder']);
