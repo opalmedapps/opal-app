5269230c34eba63c2d558cf499db3c92a4497f10
Robert Maglieri <robert.maglieri@gmail.com>
2016-09-02 13:08:18 -0400

    Changes to password reset

--- www/js/controllers/forgotPasswordController.js
+++ www/js/controllers/forgotPasswordController.js
@@ -2,57 +2,54 @@
 // Author: David Herrera on Summer 2016, Email:davidfherrerar@gmail.com
 //
 var myApp=angular.module('MUHCApp');
-myApp.controller('ForgotPasswordController', ['$scope', 'FirebaseService','$state','$timeout', function ($scope,FirebaseService, $state,$timeout) {
-    $scope.email="";
-    $scope.alert={};
-    $scope.$watch('email',function()
-    {
-      if($scope.alert.hasOwnProperty('type'))
-      {
-        delete $scope.alert.type;
-        delete $scope.alert.content;
-      }
-    });
-    $scope.submitPasswordReset = function (email) {
-        var ref = firebase.database().ref('dev2/');
-        console.log(email);
-        try{
-          ref.resetPassword({
-            email: email
-          }, function(error) {
-            if (error) {
-              switch (error.code) {
-                case "INVALID_USER":
-                  console.log("The specified user account does not exist.");
-                  $timeout(function(){
-                    $scope.alert.type="danger";
-                    $scope.alert.content="INVALID_USER";
-                  });
-
-                  break;
-                default:
-                  console.log(error);
-                  $timeout(function(){
-                    $scope.alert.type="danger";
-                    $scope.alert.content="INVALID_EMAIL";
-                  });
-              }
-            } else {
-              console.log("Password reset email sent successfully!");
-              $timeout(function(){
-                $scope.alert.type="success";
-                $scope.alert.content="EMAILPASSWORDSENT";
-              });
+myApp.controller('ForgotPasswordController', ['$scope','$state','$timeout','$firebaseAuth',
+    function ($scope,$state,$timeout, $firebaseAuth) {
+        $scope.email="";
+        $scope.alert={};
+        $scope.$watch('email',function()
+        {
+            if($scope.alert.hasOwnProperty('type'))
+            {
+                delete $scope.alert.type;
+                delete $scope.alert.content;
             }
-          });
-        }catch(err){
-          console.log(err);
-          $timeout(function(){
-            $scope.alert.type="danger";
-            $scope.alert.content="INVALID_EMAIL";
-          });
-        }
+        });
+        $scope.submitPasswordReset = function (email) {
+            var userAuth = $firebaseAuth();
+            console.log(email);
+            userAuth.$sendPasswordResetEmail(email).then(function(){
+                console.log("Password reset email sent successfully!");
+                $timeout(function(){
+                    $scope.alert.type="success";
+                    $scope.alert.content="EMAILPASSWORDSENT";
+                });
+            }).catch(function(error){
+                switch (error.code) {
+                    case "auth/user-not-found":
+                        console.log("The specified user account does not exist.");
+                        $timeout(function(){
+                            $scope.alert.type="danger";
+                            $scope.alert.content="INVALID_USER";
+                        });
+
+                        break;
+                    case "auth/invalid-email":
+                        console.log("The email is invalid.");
+                        $timeout(function(){
+                            $scope.alert.type="danger";
+                            $scope.alert.content="INVALID_EMAIL";
+                        });
+
+                        break;
+                    default:
+                        console.log(error);
+                        $timeout(function(){
+                            $scope.alert.type="danger";
+                            $scope.alert.content="INVALID_EMAIL";
+                        });
+                }
+            });
 
 
-    };
-}]);
+        };
+    }]);
