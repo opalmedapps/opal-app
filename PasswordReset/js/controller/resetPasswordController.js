302fdd9c456e62e23652cf46027d3eb107d7fb43
Robert Maglieri <robert.maglieri@gmail.com>
2016-09-08 15:17:11 -0400

    Password reset and error handling complete.

--- PasswordReset/js/controller/resetPasswordController.js
+++ PasswordReset/js/controller/resetPasswordController.js
@@ -23,7 +23,7 @@ myApp.controller('resetController',['firebase','$firebaseAuth','$location','$sco
                 break;
             default:
                 // TODO error handling for wrong mode. Tell user.
-                $scope.isNotValid = true;
+                $scope.alert.show = true;
                 $scope.alert.type='danger';
                 $scope.alert.content = "INVALID_REQUEST";
                 console.log('Invalid mode. Got ' + queryStringParameters.mode);
@@ -39,7 +39,8 @@ myApp.controller('resetController',['firebase','$firebaseAuth','$location','$sco
                     $scope.accountEmail = email;
                     requestService.setUserEmail(email);
                 }).catch(function (error) {
-                    $scope.isNotValid = true;
+                    $scope.alert.show = true;
+                    $scope.alert.type = 'danger';
                     console.log(error.code);
                     switch (error.code){
                         case "auth/expired-action-code":
@@ -71,7 +72,7 @@ myApp.controller('resetController',['firebase','$firebaseAuth','$location','$sco
 
             } catch (error) {
                 $scope.alert.type='danger';
-                $scope.isNotValid = true;
+                $scope.alert.show = true;
                 console.log(error.code);
                 $scope.alert.content = error.code;
             }
@@ -81,10 +82,12 @@ myApp.controller('resetController',['firebase','$firebaseAuth','$location','$sco
             if(ssn===''||typeof ssn=='undefined')
             {
                 $scope.alert.type='danger';
+                $scope.alert.show = true;
                 $scope.alert.content="ERRORENTERSSNNUMBER";
                 return false;
             }else if(ssn.length!==12){
                 $scope.alert.type='danger';
+                $scope.alert.show = true;
                 $scope.alert.content="ERRORENTERVALIDSSN";
                 return false;
             }
@@ -92,15 +95,19 @@ myApp.controller('resetController',['firebase','$firebaseAuth','$location','$sco
         }
 
         $scope.submitSSN = function(ssn){
+            $scope.alert.show = false;
             $scope.ssn = ssn;
             if(validateSSN(ssn)) {
+                $scope.loading = true;
                 requestService.submitSSNToServer(ssn).then(function (question) {
-                    $scope.success = true;
                     $scope.question = question;
+                    $scope.loading = false;
                     $timeout(function () {
                         $scope.template = './templates/question.html';
                     });
                 }).catch(function(error){
+                    $scope.loading = false;
+                    $scope.alert.show = true;
                     $scope.alert.type='danger';
                     $scope.alert.content=error.code;
                 });
@@ -108,42 +115,64 @@ myApp.controller('resetController',['firebase','$firebaseAuth','$location','$sco
         };
 
         $scope.submitAnswer = function (answer){
-
+            $scope.alert.show = false;
             if(!answer||answer===''||typeof answer=='undefined')
             {
+                $scope.alert.show = true;
                 $scope.alert.type='danger';
                 $scope.alert.content='ENTERANANSWER';
             }else {
                 requestService.setUserAnswer(answer);
+                $scope.loading = true;
                 requestService.submitAnswerToServer($scope.question, $scope.ssn).then(function (data) {
-                    $scope.success = true;
-                    $timeout(function () {
-                        $scope.template = './templates/newpassword.html';
-                    });
+                    $scope.loading = false;
+                    console.log(data);
+                    if(data.Data.AnswerVerified=="true") {
+                        console.log("Successfully verified answer to question.")
+                        $timeout(function () {
+                            $scope.template = './templates/newpassword.html';
+                        });
+                    } else {
+                        $scope.alert.show = true;
+                        $scope.alert.type='danger';
+                        $scope.alert.content='INCORRECT_ANSWER';
+                    }
                 }).catch(function (error) {
-                    $scope.success = false;
                     $scope.alert = error.alert;
+                    $scope.alert.show = true;
                 })
             }
         };
 
         $scope.submitNewPassword = function (newPassword) {
-
+            $scope.alert.show = false;
             if(newPassword===''||typeof newPassword=='undefined')
             {
+                $scope.alert.show = true;
                 $scope.alert.type='danger';
                 $scope.alert.content = "ENTERVALIDPASSWORD";
             }else {
-
+                $scope.loading = true;
                 auth.confirmPasswordReset(queryStringParameters.oobCode, newPassword).then(function (response) {
                     // TODO show success, link to open app
                     console.log("Successfully changed password on firebase");
                     requestService.submitNewPasswordToServer(newPassword).then(function (data) {
+                        $timeout(function () {
+                            $scope.template = '';
+                        });
                         $scope.alert = data.alert;
+                        $scope.alert.show = true;
                         console.log("Successfully changed password on opal");
-                    })
+
+                    }).catch(function(error){
+                        $scope.loading = true;
+                        $scope.alert = error.alert;
+                        $scope.alert.show = true;
+                    });
 
                 }).catch(function (error) {
+                    $scope.alert.show = true;
+                    $scope.loading = true;
                     switch (error.code) {
                         case "auth/expired-action-code":
                             $timeout(function () {
