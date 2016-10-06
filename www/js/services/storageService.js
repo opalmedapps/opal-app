b82de5e5d178b72693fc6b55fe3d7ae313211114
Robert Maglieri <robert.maglieri@gmail.com>
2016-10-06 12:48:18 -0400

    [Beta v0.1] - Created a tasks service. Created a more generic storage writer service.

---
+++ www/js/services/storageService.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('MUHCApp')
+        .factory('Storage', Storage);
+
+    Storage.$inject = ['EncryptionService'];
+
+    /* @ngInject */
+    function Storage(EncryptionService) {
+        var service = {
+            write: write,
+            read: read,
+            hasKey: hasKey,
+            remove: remove
+        };
+        return service;
+
+        ////////////////
+
+        function write(key, value) {
+            localStorage.setItem(key, EncryptionService.encryptData(JSON.stringify(value)));
+        }
+
+        function read(key){
+            return JSON.parse(EncryptionService.decryptData(localStorage.getItem(key)));
+        }
+
+        function hasKey(key) {
+            return localStorage.hasOwnProperty(key);
+        }
+
+        function remove(key) {
+            localStorage.removeItem(key);
+        }
+    }
+
+})();
+
