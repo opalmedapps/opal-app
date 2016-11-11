3868b6bf56ade81484d00ab3e0261de4794b424a
David Herrera <davidfherrerar@gmail.com>
2016-11-11 17:25:26 -0500

    Adding push notification project

---
+++ push-project/config.php
@@ -0,0 +1,12 @@
+<?php
+ //Database Credentials
+ define("DATABASE_HOST","172.26.66.41");
+ define("DATABASE_NAME","OpalDB");
+ define("DATABASE_USERNAME","root");
+ define("DATABASE_PASSWORD","root");
+
+ //Push Notification FCM and APN credientials.
+ define("API_KEY","AIzaSyC6cXQg8F7LccW-goH48l46BdfZ3YTZUnk");
+ define("CERTIFICATE_PASSWORD","v_y_d100");
+ define("CERTIFICATE_FILE",'CertificatesOpal.pem');
+?>
