ed08c6e1806d2b0fac419ff8402a2188cbb2eff2
Robert Maglieri <robert.maglieri@gmail.com>
2016-11-14 19:48:57 -0500

    Fix for some traslation on login page. Checkin system almost complete.

--- www/js/controllers/checkInController.js
+++ www/js/controllers/checkInController.js
@@ -5,20 +5,25 @@
         .module('MUHCApp')
         .controller('CheckInController', CheckInController);
 
-    CheckInController.$inject = ['CheckinService', 'NavigatorParameters', 'UserPreferences', '$q'];
+    CheckInController.$inject = ['CheckinService', 'NavigatorParameters', 'UserPreferences',
+        '$q', 'Appointments'];
 
     /* @ngInject */
-    function CheckInController(CheckinService, NavigatorParameters, UserPreferences, $q) {
+    function CheckInController(CheckinService, NavigatorParameters, UserPreferences, $q, Appointments) {
         var vm = this;
         vm.title = 'CheckInController';
         vm.apps = [];
         vm.language = 'EN';
+        vm.reponse = '';
         vm.goToAppointment = goToAppointment;
+        vm.checkInToAll = checkInToAll;
+
         activate();
 
         ////////////////
 
         function activate() {
+            verifyCheckIn(Appointments.getCheckinAppointment());
             vm.apps = NavigatorParameters.getParameters().Post;
             console.log(vm.apps);
             vm.language = UserPreferences.getLanguage();
@@ -40,9 +45,10 @@
             }
 
             $q.all(promises).then(function (dataArray) {
-
+                vm.response = "SUCCESSFULL_CHECKIN";
+                checkInButton.setDisabled(true);
             }).catch(function (error) {
-
+                vm.response = "UNSUCCESSFULL_CHECKIN";
             });
             
         }
@@ -56,9 +62,11 @@
             }
 
             $q.all(promises).then(function (dataArray) {
-                
+                checkInButton.setDisabled(false);
+                console.log("Success checkin verify", dataArray);
             }).catch(function (error) {
-                
+                vm.error = "SERVERPROBLEM";
+                console.log("No success on verify checkin", dataArray);
             });
         }
     }
