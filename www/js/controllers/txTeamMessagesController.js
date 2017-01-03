9be81d263cc123468b0166f301cfff96c98bb11f
Robert Maglieri <robert.maglieri@gmail.com>
2017-01-03 11:05:02 -0500

    Reorder TXmessages and announcements. Issue #148

--- www/js/controllers/txTeamMessagesController.js
+++ www/js/controllers/txTeamMessagesController.js
@@ -1,37 +1,46 @@
 /*
-*Code by David Herrera May 20, 2015
-*Github: dherre3
-*Email:davidfherrerar@gmail.com
-*/
+ *Code by David Herrera May 20, 2015
+ *Github: dherre3
+ *Email:davidfherrerar@gmail.com
+ */
 var myApp=angular.module('MUHCApp');
 myApp.controller('TxTeamMessagesController',['$scope','$timeout','TxTeamMessages','UserPreferences','NavigatorParameters',function($scope,$timeout,TxTeamMessages,UserPreferences,NavigatorParameters){
 
-  //Initializing
+    //Initializing
 
 
-  init();
-  //Initializing name and body of post
-  function init()
-  {
-    $scope.noMessages = true;
-    var messages = TxTeamMessages.getTxTeamMessages();
-    messages = TxTeamMessages.setLanguageTxTeamMessages(messages);
-    if (messages.length>0) $scope.noMessages = false;
-    $scope.txTeamMessages=messages;
-  }
-  //Function that goes to individual team message
-  $scope.goToTeamMessage=function(message)
-  {
-    console.log(message);
-    if(message.ReadStatus == '0')
+    init();
+    //Initializing name and body of post
+    function init()
     {
-      console.log(message);
-      message.ReadStatus = '1';
-      TxTeamMessages.readTxTeamMessageBySerNum(message.TxTeamMessageSerNum);
+        $scope.noMessages = true;
+        var messages = TxTeamMessages.getTxTeamMessages();
+        messages = TxTeamMessages.setLanguageTxTeamMessages(messages);
+        if (messages.length>0) $scope.noMessages = false;
+        $scope.txTeamMessages=messages;
     }
-    NavigatorParameters.setParameters({'Navigator':'personalNavigator','Post':message});
-    personalNavigator.pushPage('./views/personal/treatment-team-messages/individual-team-message.html');
-  }
+    //Function that goes to individual team message
+    $scope.goToTeamMessage=function(message)
+    {
+        console.log(message);
+        if(message.ReadStatus == '0')
+        {
+            console.log(message);
+            message.ReadStatus = '1';
+            TxTeamMessages.readTxTeamMessageBySerNum(message.TxTeamMessageSerNum);
+        }
+        NavigatorParameters.setParameters({'Navigator':'personalNavigator','Post':message});
+        personalNavigator.pushPage('./views/personal/treatment-team-messages/individual-team-message.html');
+    }
+
+    $scope.showHeader = function (index) {
+        if (index === 0) return true;
+
+        var current = (new Date($scope.txTeamMessages[index].DateAdded)).setHours(0,0,0,0);
+        var previous = (new Date($scope.txTeamMessages[index-1].DateAdded)).setHours(0,0,0,0);
+        return current != previous;
+    }
+
 }]);
 myApp.controller('IndividualTxTeamMessageController',['$scope','NavigatorParameters','TxTeamMessages','Patient', function($scope,NavigatorParameters,TxTeamMessages, Patient){
     var parameters=NavigatorParameters.getParameters();
