d556cfecb218ea07ba2bfdf1f62e10fff65d4352
Robert Maglieri <robert.maglieri@gmail.com>
2017-01-24 12:48:09 -0500

    Updated patients for patients

--- www/js/controllers/feedbackController.js
+++ www/js/controllers/feedbackController.js
@@ -9,7 +9,7 @@ myApp.controller('FeedbackController',['Patient', 'RequestToServer','$scope', fu
 	$scope.profilePicture=Patient.getProfileImage();
 	$scope.enableSend=false;
 	$scope.$watch('feedbackText',function(){
-		if(($scope.feedbackText===''||!$scope.feedbackText)||$scope.emptyRating)
+		if(($scope.feedbackText===''||!$scope.feedbackText))
 		{
 			$scope.enableSend=false;
 			if(!$scope.emptyRating) $scope.enableSend=true;
