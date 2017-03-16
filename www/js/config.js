b895e366067ce22ebd41ab8c20dd6628e612ecc4
Robert Maglieri <robert.maglieri@gmail.com>
2017-03-16 15:08:17 -0400

    Partial merge

---
+++ www/js/config.js
@@ -0,0 +1,11 @@
+myApp.config(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCxV6VuVWtKg5wkeReqzQNfRULwkvVXkos",
+        authDomain: "brilliant-inferno-7679.firebaseapp.com",
+        databaseURL: "https://brilliant-inferno-7679.firebaseio.com",
+        storageBucket: "firebase-brilliant-inferno-767.appspot.com",
+        messagingSenderId: "810896751588"
+    };
+    firebase.initializeApp(config);
+});
