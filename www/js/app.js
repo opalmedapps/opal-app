1671f8000bf63125358390d69aff3b31bf8d07a6
Robert Maglieri <robert.maglieri@gmail.com>
2017-03-16 11:53:57 -0400

    Moving out configs.

--- www/js/app.js
+++ www/js/app.js
@@ -182,18 +182,6 @@ myApp.config(function ($translateProvider) {
     $translateProvider.useMissingTranslationHandlerLog();
 });
 
-myApp.config(function () {
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyCw4vvsyYYHbzKisZdJ3CdOOwClkG10J0A",
-        authDomain: "opal-mactest.firebaseapp.com",
-        databaseURL: "https://opal-mactest.firebaseio.com",
-        storageBucket: "opal-mactest.appspot.com",
-        messagingSenderId: "634611685707"
-    };
-    firebase.initializeApp(config);
-});
-
 myApp.config(['$qProvider', function ($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
 }]);
