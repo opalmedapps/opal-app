27deb16c2fd653eee60fbbf2ff5bcb41ff819cdf
Robert Maglieri <robert.maglieri@gmail.com>
2017-04-13 11:31:09 -0400

    Update configs

--- www/js/config.js
+++ www/js/config.js
@@ -5,11 +5,11 @@ fireConfig.$inject = [];
 /* @ngInject */
 function fireConfig () {
     var config = {
-        apiKey: "AIzaSyCw4vvsyYYHbzKisZdJ3CdOOwClkG10J0A",
-        authDomain: "opal-mactest.firebaseapp.com",
-        databaseURL: "https://opal-mactest.firebaseio.com",
-        storageBucket: "opal-mactest.appspot.com",
-        messagingSenderId: "634611685707"
+        apiKey: "AIzaSyCxV6VuVWtKg5wkeReqzQNfRULwkvVXkos",
+        authDomain: "brilliant-inferno-7679.firebaseapp.com",
+        databaseURL: "https://brilliant-inferno-7679.firebaseio.com",
+        storageBucket: "firebase-brilliant-inferno-767.appspot.com",
+        messagingSenderId: "810896751588"
     };
     firebase.initializeApp(config);
 }
