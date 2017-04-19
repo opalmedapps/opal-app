e737932a4ec6af70fe24cd1242c8dcffa4b31b67
Robert Maglieri <robert.maglieri@gmail.com>
2017-04-19 14:15:57 -0400

    Data is loaded for notification access

--- opal-labs/js/app.js
+++
@@ -1,9 +0,0 @@
-
-                  ons.ready(function() {
-            console.log("Onsen UI is ready!");
-        });
-var app = angular.module('myApp', ['onsen']);
-app.controller('AppController', function() {
-            this.pushes = 0;
-            this.pops = 0;
-          });
