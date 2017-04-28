c37b7ae94e28b3160f5b227171707221eab76f18
Robert Maglieri <robert.maglieri@gmail.com>
2017-04-28 12:22:49 -0400

    Updates to controller style.

--- www/js/controllers/infoTabController.js
+++ www/js/controllers/infoTabController.js
@@ -1,40 +1,64 @@
-//
-// Author: David Herrera on Summer 2016, Email:davidfherrerar@gmail.com
-//
-var myApp=angular.module('MUHCApp');
-myApp.controller('InfoTabController',['$scope','$timeout','$filter','$sce',function($scope,$timeout,$filter,$sce){
-  var tab=tabbar.getActiveTabIndex();
-  console.log('inside tabs controller');
-  var views=[
-    {
-      icon:'fa fa-home',
-      color:'SteelBlue',
-      name:"HOME",
-      description:"HOME_DESCRIPTION"
-    },
-    {
-      icon:'ion-android-person',
-      color:'maroon',
-      name:"MYCHART" ,
-      description:"MYCHART_DESCRIPTION"
-    },
-    {
-      icon:'ion-ios-book',
-      color:'darkblue',
-      name: "GENERAL",
-      description:"GENERAL_DESCRIPTION"
-    },
-    {
-      icon:'ion-university',
-      color:'Chocolate',
-      name:"EDUCATION",
-      description:"EDUCATION_DESCRIPTION"
-    },
-  ];
-  $scope.trustAsHtml = function(string) {
-    return $sce.trustAsHtml(string);
-};
-  $scope.view = views[tab];
-  $scope.view.description = $filter('translate')($scope.view.description );
-  console.log($scope.view.description);
-  }]);
+/*
+ * Filename     :   infoTabController.js
+ * Description  :   Manages the information view.
+ * Created by   :   David Herrera, Robert Maglieri 
+ * Date         :   28 Apr 2017
+ * Copyright    :   Copyright 2016, HIG, All rights reserved.
+ * Licence      :   This file is subject to the terms and conditions defined in
+ *                  file 'LICENSE.txt', which is part of this source code package.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('MUHCApp')
+        .controller('InfoTabController', InfoTabController);
+
+    InfoTabController.$inject = ['$timeout','$filter','$sce'];
+
+  /* @ngInject */
+    function InfoTabController($timeout,$filter,$sce) {
+        var vm = this;
+        vm.title = 'InfoTabController';
+        vm.view = {};
+
+        var views=[
+            {
+                icon:'fa-home',
+                color:'SteelBlue',
+                name:"HOME",
+                description:"HOME_DESCRIPTION"
+            },
+            {
+                icon:'fa-user',
+                color:'maroon',
+                name:"MYCHART" ,
+                description:"MYCHART_DESCRIPTION"
+            },
+            {
+                icon:'fa-th',
+                color:'darkblue',
+                name: "GENERAL",
+                description:"GENERAL_DESCRIPTION"
+            },
+            {
+                icon:'fa-mortar-board',
+                color:'Chocolate',
+                name:"EDUCATION",
+                description:"EDUCATION_DESCRIPTION"
+            }
+        ];
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            var tab=tabbar.getActiveTabIndex();
+            vm.view = views[tab];
+            vm.view.description = $filter('translate')(vm.view.description );
+        }
+    }
+
+})();
