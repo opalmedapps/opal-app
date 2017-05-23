84dcecfbbe7bcb6d94f178a3017df238279d903e
wen <wenquan97@gmail.com>
2017-05-23 16:53:07 -0400

    waiting time feature updated with select

--- www/js/controllers/waitController.js
+++ www/js/controllers/waitController.js
@@ -21,21 +21,15 @@ var myApp = angular.module('MUHCApp');
 
 
 //Logic for the calendar controller view
-myApp.controller('waitController', ['Appointments', '$q','$scope','$timeout', '$filter', '$location',
-    '$templateRequest', '$sce', '$compile','$anchorScroll','NavigatorParameters', 'UserPreferences', 'Logger',
-    function (Appointments, $q, $scope,$timeout,$filter,$location,$templateRequest, $sce, $compile,
-              $anchorScroll,NavigatorParameters,UserPreferences, Logger) {
+myApp.controller('waitController', ['$scope','$timeout', '$filter','$anchorScroll','NavigatorParameters', 'UserPreferences',
+    function ($scope,$timeout,$filter,
+              $anchorScroll,NavigatorParameters,UserPreferences) {
 
         /*
          *   Controller constants
          **/
         var flag;//Boolean value to indicate initialization
-        var todaysTimeMilliseconds;//Time in milliseconds
-        var choosenTimeMilliseconds;//Selected time in milliseconds
         var today;//Date today
-        var dateLast; //Date of last Appointment;
-        var dateFirst;//Date of first Appointment;
-        var navigatorName;//Navigator Name;
         //Set the calendar options
         $scope.dateOptions = {
             formatYear: 'yyyy',
@@ -43,237 +37,69 @@ myApp.controller('waitController', ['Appointments', '$q','$scope','$timeout', '$
             formatDay:'d',
             showWeeks:false
         };
-        //Determines color for calendar dates based on appointments
-        $scope.showColor = showColor;
-        //Obtains color for a given appointment
-        $scope.getStyle=getStyle;
-        //Determines whether to show header at the end
-        $scope.showHeaderEnd = showHeaderEnd;
-        //Determines whether or not to show red highlighted date per index
-        $scope.showChoosenDateHeader=showChoosenDateHeader;
-        //Go to Appointment
-        $scope.goToAppointment=goToAppointment;
-        //Go to Calendar options
-        $scope.goToCalendarOptions = goToCalendarOptions;
-
-        //monitors whether or not the calendar is displayed
+        //monitors whether or not the calendar is displayed, here the calendar should always be displayed
         $scope.showCalendar = true;
-
-        $scope.scrollerHeight = '';
-
-        $scope.onHideCalendar= function() {
-
-            $scope.$apply(function() {
-                $scope.showCalendar = false;
-                $scope.scrollerHeight = '100%';
-            })
-        };
-
         $scope.onShowCalendar= function() {
             $scope.$apply(function() {
                 $scope.showCalendar = true;
-                $scope.scrollerHeight = '45%';
             })
         };
+        var vm = this;
+        vm.Language = UserPreferences.getLanguage();
 
         /*
          *   Implementation
          **/
         //Initializing controller
         init();
+        //changeSetUp();
 
-        function init()
-        {
-            Logger.sendLog('Appointment', 'all');
-            navigatorName = NavigatorParameters.getParameters().Navigator;
+        function init() {
+            //navigatorName = NavigatorParameters.getParameters().Navigator;
 
             //Obtaining and setting appointments from service
-            $scope.appointments=Appointments.getUserAppointments();
-            $scope.noAppointments = ($scope.appointments.length === 0);
-            $scope.loading = !$scope.noAppointments;
             $scope.language = UserPreferences.getLanguage();
             $scope.dt = new Date();
-            $scope.dt.setHours(0,0,0,0);
+            $scope.dt.setHours(0, 0, 0, 0);
             today = new Date($scope.dt);
-            flag=false;
-            //Getting time in milliseconds for today's appointment
-            todaysTimeMilliseconds =  today.getTime();
-            choosenTimeMilliseconds = todaysTimeMilliseconds;
-            if($scope.appointments.length>0)
-            {
-                //Setting time in milliseconds for last appointment
-                dateLast=(new Date($scope.appointments[$scope.appointments.length-1].ScheduledStartTime.getTime()));
-                dateLast.setHours(0,0,0,0);
-                dateLast = dateLast.getTime();
-                //Setting time in milliseconds for first appointment
-                dateFirst=(new Date($scope.appointments[0].ScheduledStartTime.getTime()));
-                dateFirst.setHours(0,0,0,0);
-                dateFirst = dateFirst.getTime();
-            }
-            loadTemplate().then(function(){
-                var divTreatment=document.getElementById('scrollerAppointments');
-                var heightTreatment=document.documentElement.clientHeight-document.documentElement.clientHeight*0.35-180;
-                if(divTreatment)divTreatment.style.height=heightTreatment+'px';
-                setTimeout(scrollToAnchor,100);
-            });
-        }
-        function scrollToAnchor()
-        {
-            var anchor=findClosestAnchor();
-            $location.hash(anchor);
-            $anchorScroll();
+            flag = false;
         }
 
-        function loadTemplate()
-        {
-            var r = $q.defer();
-            $timeout(function () {
-                $scope.loading = false;
-                var template= $sce.getTrustedResourceUrl('./views/personal/appointments/appointment-list-template.html');
-                $templateRequest(template).then(function(template) {
-                    $compile($("#appointment-list").html(template).contents())($scope);
-                    r.resolve(true);
-                }, function() {
-                    r.resolve(true);
-                    // An error has occurred
+
+        //activate();
+
+                function activate() {
+
+                loadSettings();
+
+                // Setting our parameters for pushing and popping pages
+                NavigatorParameters.setParameters({
+                    'Navigator':'homeNavigator'
                 });
-            }, 1000);
-            return r.promise;
-        }
-        function goToCalendarOptions()
-        {
-            window[navigatorName].pushPage('./views/personal/appointments/calendar-options.html');
-        }
-        //Watcher for the calendar date
-        $scope.$watch('dt',function(){
-            choosenTimeMilliseconds = $scope.dt.getTime();
-            scrollToAnchor();
-        });
 
-        //Determines color for calendar dates based on appointments
-        function showColor(date)
-        {
-            var result = $scope.appointments.find(function(item){
-                return  item.ScheduledStartTime.toDateString() == date.toDateString();
-            });
-            if(result)
-            {
-                if(date.getTime()> todaysTimeMilliseconds)
-                {
-                    return '#cf5c4c';
-                }else if(date.getTime() == todaysTimeMilliseconds)
-                {
-                    return '#3399ff';
-                }else{
-                    return '#5CE68A';
-                }
-            }else{
-                return 'rgba(255,255,255,0.0)';
+                // After a page is popped reintialize the settings.
+                homeNavigator.on('postpop', function() {
+                    $timeout(function() {
+                        loadSettings();
+                    });
+
+                });
+
+                //On destroy, dettach listener
+                $scope.$on('destroy', function() {
+                    homeNavigator.off('postpop');
+                });
+
             }
-        }
 
-
-        //Returns string with closest anchor
-        function findClosestAnchor()
-        {
-            if($scope.appointments.length>0)
-            {
-                if(dateLast<choosenTimeMilliseconds) return 'lastAnchor';
-                else if(dateFirst>choosenTimeMilliseconds) return 'firstAnchor';
-                else{
-                    var ind = findClosestAppointmentToTime(choosenTimeMilliseconds);
-                    return 'anchorAppointments'+ ind;
-                }
+            function accountDeviceBackButton() {
+                tabbar.setActiveTab(0);
             }
-            return 'firstAnchor';
-        }
-        //Finds the closest appointment that happens after the choosen appointment, this is use for scrolling
-        //Could be done with binary search for an improvement
-        function findClosestAppointmentToTime(tmili)
-        {
-            for(var i =0;i<$scope.appointments.length;i++)
-            {
-                var date = $scope.appointments[i].ScheduledStartTime.getTime();
-                if(date >= tmili)
-                {
-                    return i;
-                }
-            }
-            return 0;
-            //To be added once is supported by all browsers!
-            // var ind = 0;
-            // ind = $scope.appointments.findIndex(function(appointment)
-            // {
-            //     var date = appointment.ScheduledStartTime.getTime();
-            //     return date >= tmili;
 
-            // });
-            // return ind;
-        }
-
-        //Obtains color for a given appointment
-        function getStyle(index){
-            var dateAppointment=$scope.appointments[index].ScheduledStartTime;
-            if(today.getDate()===dateAppointment.getDate()&&today.getMonth()===dateAppointment.getMonth()&&today.getFullYear()===dateAppointment.getFullYear()){
-                return '#3399ff';
-            }else if(dateAppointment>today){
-                return '#cf5c4c';
-            }else{
-                return '#5CE68A';
+            function loadSettings() {
+                vm.Language = UserPreferences.getLanguage();
             }
-        }
 
-        //Determines whether to show header at the end
-        function showHeaderEnd()
-        {
-            if($scope.appointments.length>0&&dateLast<choosenTimeMilliseconds)return true;
-            return false;
-        }
-        //Determines when to show header of appointment vs red highlighted header
-        function showChoosenDateHeader(index)
-        {
-            var date2=new Date($scope.appointments[index].ScheduledStartTime);
-            date2.setHours(0,0,0,0);
-            date2=date2.getTime();
-            $scope.showHeaderNormalDay = true;
-            if(choosenTimeMilliseconds==date2)
-            {
-                $scope.showHeaderNormalDay = false;
-            }
-            if(index==0)
-            {
-                if(choosenTimeMilliseconds==date2||choosenTimeMilliseconds<date2)
-                {
-                    return true
-                }else{
-                    return false;
-                }
-            }else
-            {
-                var date1=new Date($scope.appointments[index-1].ScheduledStartTime.getTime());
-                date1.setHours(0,0,0,0);
-                date1=date1.getTime();
-                if(date1==date2)
-                {
-                    return false;
-                }else{
-                    if((date2>choosenTimeMilliseconds&&date1<choosenTimeMilliseconds)||date2==choosenTimeMilliseconds)
-                    {
-                        return true;
-                    }else{
-                        return false;
-                    }
-                }
-            }
-        }
-
-        //Leaves to appointment
-        function goToAppointment(appointment)
-        {
-            if(appointment.ReadStatus == '0') Appointments.readAppointmentBySerNum(appointment.AppointmentSerNum);
-            NavigatorParameters.setParameters({'Navigator':navigatorName, 'Post':appointment});
-            window[navigatorName].pushPage('./views/personal/appointments/individual-appointment.html');
-        }
 
     }]);
 
@@ -308,4 +134,92 @@ myApp.controller('IndividualAppointmentController', ['NavigatorParameters','Nati
         }
 
     }]);
+
+myApp.controller('waitChangeController',
+    function($filter, $rootScope, $translate, UserPreferences, $scope) {
+        //Function sets account
+        changeSetUp();
+
+        //Sets all the account settings depeding on the field that needs to be changed
+        function changeSetUp() {
+            //Mappings between parameters and translation
+            //Navigator parameter
+            var page = homeNavigator.getCurrentPage();
+            var parameters = page.options.param;
+
+            //Instantiates values and parameters
+            $scope.disableButton = true;
+            $scope.title = 'UPDATE';
+            $scope.value = parameters;
+            $scope.valueLabel = parameters;
+            $scope.timeUpdated = true;
+            $scope.type1 = 'text';
+
+
+            //Sets the instructions depending on the value to update
+            if (parameters === 'HOUR') {
+                $scope.timeUpdated = false;
+                $scope.typeUpdated = false;
+                $scope.firstOption = '8';
+                $scope.secondOption = '9';
+                $scope.thirdOption = '10';
+                $scope.forthOption = '11';
+                $scope.fifthOption = '12';
+                $scope.sixthOption = '13';
+                $scope.seventhOption = '14';
+                $scope.eigthOption = '15';
+                $scope.ninthOption = '16';
+                $scope.valueLabel = 'HOUR';
+                $scope.title = 'UPDATE';
+                $scope.instruction = 'SELECTHOUR';
+            } else if (parameters === 'MINUTE') {
+                $scope.instruction = 'SELECTMINUTES';
+                $scope.timeUpdated = false;
+                $scope.typeUpdated = true;
+                $scope.firstOption = '00';
+                $scope.secondOption = '15';
+                $scope.thirdOption = '30';
+                $scope.forthOption = '45';
+                $scope.valueLabel = 'MINUTES';
+                $scope.title = 'UPDATE';
+                //$scope.pickMinute = value;
+            } else if (parameters === 'TYPE') {
+                $scope.firstOption = 'chemoapt';
+                $scope.secondOption = 'chemotx';
+                $scope.thirdOption = 'xlarge';
+                $scope.instruction = 'SELECTTYPE';
+                $scope.timeUpdated = true;
+                $scope.typeUpdated = true;
+            }
+
+        //Function to update new value
+        $scope.updateValue = function(val) {
+            var objectToSend = {};
+            objectToSend.NewValue = $scope.newValue;
+
+            if (val == 'Password') {
+                changePassword();
+            } else if (val == 'Email') {
+                changeEmail();
+            }else{
+                changeField(val, $scope.newValue);
+            }
+            $scope.disableButton = true;
+        };
+
+        //Function to change font size
+        $scope.changeType = function(newVal) {
+
+        };
+
+
+        //FUnction to change the language
+        $scope.changeHour = function(val) {
+
+        };
+
+        $scope.changeMinute = function(val) {
+
+        }
+    }});
 
