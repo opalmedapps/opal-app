70646c0a4427673eee694784cf666275db772946
James Brace <bracejames95@gmail.com>
2017-05-29 13:06:58 -0400

    fix config for master

--- www/js/config.js
+++ www/js/config.js
@@ -5,11 +5,13 @@ fireConfig.$inject = [];
 /* @ngInject */
 function fireConfig () {
     var config = {
-        apiKey: "AIzaSyCxV6VuVWtKg5wkeReqzQNfRULwkvVXkos",
-        authDomain: "brilliant-inferno-7679.firebaseapp.com",
-        databaseURL: "https://brilliant-inferno-7679.firebaseio.com",
-        storageBucket: "firebase-brilliant-inferno-767.appspot.com",
-        messagingSenderId: "810896751588"
+        apiKey: "AIzaSyAGBV2Zwr0SYKy_J92jceof-OFeeFmm0Gw",
+        authDomain: "opal-prod.firebaseapp.com",
+        databaseURL: "https://opal-prod.firebaseio.com",
+        projectId: "opal-prod",
+        storageBucket: "opal-prod.appspot.com",
+        messagingSenderId: "476395494069"
     };
+    
     firebase.initializeApp(config);
 }
