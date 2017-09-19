46d4a4c5167d15dafabd6bedbd313916145521af
James Brace <bracejames95@gmail.com>
2017-09-19 12:19:45 -0400

    Refactor maps, notifications, and parking

---
+++ www/js/controllers/maps/mapsController.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('MUHCApp')
+        .controller('MapController', MapController);
+
+    MapController.$inject = ['NavigatorParameters','UserPreferences'];
+
+    /* @ngInject */
+    function MapController(NavigatorParameters,UserPreferences) {
+        var vm = this;
+        var language;
+
+        vm.openMap = openMap;
+
+        activate();
+
+        ///////////////////////
+
+        function activate(){
+            vm.map=NavigatorParameters.getParameters();
+            language = UserPreferences.getLanguage().toUpperCase();
+
+            if(language === 'EN')
+            {
+                vm.name=vm.map.MapName_EN;
+                vm.description=vm.map.MapDescription_EN;
+            } else {
+                vm.name=vm.map.MapName_FR;
+                vm.description=vm.map.MapDescription_FR;
+            }
+        }
+
+        function openMap(){
+            var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+            if(app)
+            {
+                cordova.InAppBrowser.open(vm.map.MapUrl, '_blank', 'EnableViewPortScale=yes');
+            } else {
+                window.open(vm.map.MapUrl);
+            }
+        }
+    }
+})();
