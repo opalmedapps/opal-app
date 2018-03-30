f77f1403d685864cc65929e68a4a8ac0ed356449
James Brace <bracejames95@gmail.com>
2018-03-30 10:34:52 -0400

    Add documentation for security service

--- www/js/services/securityService.js
+++ www/js/services/securityService.js
@@ -1,3 +1,11 @@
+/**
+ * __author__ : James Brace
+ *
+ * The purpose of this service is to preserve the security state of the app. Security is defined in an object literal
+ * called 'security_state'. Each state field consists of another object literal that consists of the 'value' of the
+ * sub-state .. ie can take on value 'true' for secure and 'false' for insecure .. and the 'callbacks' which is an array
+ * of functions that controllers can bind to in order to monitor changes in security state
+ */
 (function () {
     'use strict';
 
@@ -35,6 +43,7 @@
             security_state[state].callbacks.push(callback);
         }
 
+        // update the state, if insecure notify the observers via their callbacks
         function updateState(state, value){
             security_state[state].value = value;
 
@@ -43,7 +52,8 @@
             }
         }
 
-        //call this when you know state has been changed
+        //notify observers about insecure state
+        // TODO: perhaps reflect on situations where you want to notify on 'secure' state
         function notifyObservers(state){
             security_state[state].callbacks.forEach(callback => {
                 callback();
