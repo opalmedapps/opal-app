ad3d8ad0d4d0c0509640e1ba24e06fa0560aa8c3
Khodr <khodr007@gmail.com>
2018-06-20 19:57:20 -0400

    config.js

--- www/js/config.js
+++ www/js/config.js
@@ -6,19 +6,12 @@ fireConfig.$inject = [];
 function fireConfig () {
 
     var config = {
-        apiKey: "AIzaSyAGBV2Zwr0SYKy_J92jceof-OFeeFmm0Gw",
-        authDomain: "opal-prod.firebaseapp.com",
-        databaseURL: "https://opal-prod.firebaseio.com",
-        projectId: "opal-prod",
-        storageBucket: "opal-prod.appspot.com",
-        messagingSenderId: "476395494069"
-
-        // // PreProd (Dev)
-        // apiKey: "AIzaSyCxV6VuVWtKg5wkeReqzQNfRULwkvVXkos",
-        // authDomain: "brilliant-inferno-7679.firebaseapp.com",
-        // databaseURL: "https://brilliant-inferno-7679.firebaseio.com",
-        // storageBucket: "firebase-brilliant-inferno-767.appspot.com",
-        // messagingSenderId: "810896751588"
+        // PreProd (Dev)
+        apiKey: "AIzaSyCxV6VuVWtKg5wkeReqzQNfRULwkvVXkos",
+        authDomain: "brilliant-inferno-7679.firebaseapp.com",
+        databaseURL: "https://brilliant-inferno-7679.firebaseio.com",
+        storageBucket: "firebase-brilliant-inferno-767.appspot.com",
+        messagingSenderId: "810896751588"
     };
 
     if (!firebase.apps.length) {
