d8c431429d91066085126086831df8418ad65ab7
Khodr Ghattas <khodr007@gmail.com>
2018-08-13 16:33:48 -0400

    config

--- www/js/controllers/main/mainController.js
+++ www/js/controllers/main/mainController.js
@@ -12,8 +12,8 @@
         .controller('MainController', MainController);
 
     MainController.$inject = ["$window", "$state", '$rootScope','FirebaseService','DeviceIdentifiers',
-                                '$translatePartialLoader', "LocalStorage", 'Constants', 'CleanUp',
-                                'NavigatorParameters', 'NetworkStatus', 'RequestToServer', 'NewsBanner', 'Security'];
+        '$translatePartialLoader', "LocalStorage", 'Constants', 'CleanUp',
+        'NavigatorParameters', 'NetworkStatus', 'RequestToServer', 'NewsBanner', 'Security'];
 
     /* @ngInject */
     function MainController($window, $state, $rootScope, FirebaseService, DeviceIdentifiers,
@@ -151,12 +151,9 @@
                     android: {
                         icon: "opal_notification",
                         iconColor: "#74A333",
-<<<<<<< HEAD
-                        senderID: "476395494069"
-=======
-                        senderID: "810896751588",
+                        senderID: "476395494069",   // PRODUCTION
+                        // senderID: "810896751588",   // pre-prod
                         forceShow: "true"
->>>>>>> opal_pre_prod
                     }
                 });
 
