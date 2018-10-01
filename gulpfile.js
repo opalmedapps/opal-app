02574e4e0c9263f02c0fb167de20e10bbbfa3c21
Khodr <khodr007@gmail.com>
2018-10-01 16:26:08 -0400

    Un-minify onsenui.css and its dependecies

--- gulpfile.js
+++ gulpfile.js
@@ -124,11 +124,11 @@ gulp.task('copy-vendor-css',function()
 {
     return gulp.src([
         'www/lib/bower_components/angular/angular-csp.css',
-        'www/lib/bower_components/onsenui/css/onsen-css-components-blue-theme.css',
+        // 'www/lib/bower_components/onsenui/css/onsen-css-components-blue-theme.css',
         'www/lib/bower_components/font-awesome/css/font-awesome.min.css',
         'www/lib/bower_components/bootstrap/dist/css/bootstrap.min.css',
         'www/lib/css/animate.css',
-        'www/lib/bower_components/onsenui/css/onsenui.css',
+        // 'www/lib/bower_components/onsenui/css/onsenui.css',
         'www/lib/bower_components/Ionicons/css/ionicons.min.css'
     ])
         .pipe(concat('vendor.min.css'))
