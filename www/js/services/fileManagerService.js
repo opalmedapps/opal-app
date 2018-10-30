6f4142a0e049522c70ee3c7fa31ab03588cfb8d6
Khodr Ghattas <khodr007@gmail.com>
2018-10-30 17:02:18 -0400

    Fix Add Documents to List

--- www/js/services/fileManagerService.js
+++ www/js/services/fileManagerService.js
@@ -11,7 +11,7 @@ var myApp = angular.module('MUHCApp');
  *@requires $filter
  *@description Allows the app's controllers or services interact with the file storage of the device. For more information look at {@link https://github.com/apache/cordova-plugin-file Cordova File Plugin}, reference for social sharing plugin {@link https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin Cordova Sharing Plugin}
  **/
-myApp.service('FileManagerService', function ($q, $cordovaFileTransfer, $cordovaFileOpener2, $filter, NewsBanner) {
+myApp.service('FileManagerService', function ($q, $cordovaFileTransfer, $cordovaFileOpener2, $filter, NewsBanner, Documents) {
     //Determing whether is a device or the browser
     var app = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
 
