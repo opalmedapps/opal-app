47a1401d7c33b7c5120fb134e6da030e71b12149
yickmo <yickmo@gmail.com>
2019-01-23 10:30:24 -0500

    Modified the stating script

--- bumpstaging.sh
+++ bumpstaging.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# works with a file called VERSION in the current directory,
+# Original script : bumpversion.sh and modified
+# for staging version
+
+# works with a file called STAGING in the current directory,
 # the contents of which should be a semantic version number
 # such as "1.2.3.4"
 
@@ -13,50 +16,50 @@
 # a file called CHANGES (under the title of the new version
 # number) and create a GIT tag.
 
-if [ -f VERSION ]; then
-    BASE_STRING=`cat VERSION`
+if [ -f STAGING ]; then
+    BASE_STRING=`cat STAGING`
     BASE_LIST=(`echo $BASE_STRING | tr '.' ' '`)
     V_MAJOR=${BASE_LIST[0]}
     V_MINOR=${BASE_LIST[1]}
     V_CHANGE=${BASE_LIST[2]}
     V_PATCH=${BASE_LIST[3]}
-    echo "Current version : $BASE_STRING"
+    echo "Current staging : $BASE_STRING"
     V_MINOR=$((V_MINOR + 1))
     V_CHANGE=0
     V_PATCH=0
     SUGGESTED_VERSION="$V_MAJOR.$V_MINOR.$V_CHANGE.$V_PATCH"
-    read -p "Enter a version number [$SUGGESTED_VERSION]: " INPUT_STRING
+    read -p "Enter a staging number [$SUGGESTED_VERSION]: " INPUT_STRING
     if [ "$INPUT_STRING" = "" ]; then
         INPUT_STRING=$SUGGESTED_VERSION
     fi
-    echo "Will set new version to be $INPUT_STRING"
-    echo $INPUT_STRING > VERSION
-    echo "Version $INPUT_STRING:" > tmpfile
-    git log --pretty=format:" - %s" "v$BASE_STRING"...HEAD >> tmpfile
+    echo "Will set new staging to be $INPUT_STRING"
+    echo $INPUT_STRING > STAGING
+    echo "Staging $INPUT_STRING:" > tmpfile
+    git log --pretty=format:" - %s" "st$BASE_STRING"...HEAD >> tmpfile
     echo "" >> tmpfile
     echo "" >> tmpfile
     cat CHANGES >> tmpfile
     mv tmpfile CHANGES
-    git add CHANGES VERSION
-    git commit -m "Version bump to $INPUT_STRING"
-    git tag -a -m "Tagging version $INPUT_STRING" "v$INPUT_STRING"
+    git add CHANGES STAGING
+    git commit -m "Staging bump to $INPUT_STRING"
+    git tag -a -m "Tagging staging $INPUT_STRING" "st$INPUT_STRING"
 else
-    echo "Could not find a VERSION file"
-    read -p "Do you want to create a version file and start from scratch? [y]" RESPONSE
+    echo "Could not find a STAGING file"
+    read -p "Do you want to create a staging file and start from scratch? [y]" RESPONSE
     if [ "$RESPONSE" = "" ]; then RESPONSE="y"; fi
     if [ "$RESPONSE" = "Y" ]; then RESPONSE="y"; fi
     if [ "$RESPONSE" = "Yes" ]; then RESPONSE="y"; fi
     if [ "$RESPONSE" = "yes" ]; then RESPONSE="y"; fi
     if [ "$RESPONSE" = "YES" ]; then RESPONSE="y"; fi
     if [ "$RESPONSE" = "y" ]; then
-        echo "0.1.0.0" > VERSION
-        echo "Version 0.1.0.0" > CHANGES
+        echo "0.1.0.0" > STAGING
+        echo "Staging 0.1.0.0" > CHANGES
         git log --pretty=format:" - %s" >> CHANGES
         echo "" >> CHANGES
         echo "" >> CHANGES
-        git add VERSION CHANGES
-        git commit -m "Added VERSION and CHANGES files, Version bump to v0.1.0.0"
-        git tag -a -m "Tagging version 0.1.0.0" "v0.1.0.0"
+        git add STAGING CHANGES
+        git commit -m "Added STAGING and CHANGES files, Staging bump to st0.1.0.0"
+        git tag -a -m "Tagging staging 0.1.0.0" "st0.1.0.0"
     fi
 
 fi
