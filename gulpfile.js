c856c8663f7d47aa164cd7d1b4d99e09e5a7e496
Tessa Condon <tesseract970@gmail.com>
2019-07-30 12:14:26 -0400

    Clean Up Comments and Fix Bugs in Graphs
    The delays graph gives labels now. The waiting time graph drop down menu
    doesn't have to be double clicked every time an item is selected. The
    waiting time graph options give data from X time selected to now instead
    of X time selected to the last event. Comments have been cleaned up and
    index-build has been reverted to what it was, as it's not used.

--- gulpfile.js
+++ gulpfile.js
@@ -191,7 +191,6 @@ gulp.task('minify-vendor-js',function()
         'www/lib/bower_components/highcharts/highstock.js',
         'www/lib/bower_components/highcharts/highcharts.js',
         'www/lib/bower_components/highcharts/modules/exporting.js',
-        //End TESSA
         'www/lib/bower_components/moment/moment.js',
         'www/lib/js/materialize.min.js',
         'www/lib/bower_components/crypto-js/crypto-js.js',
