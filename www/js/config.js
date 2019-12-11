4d09be9956731e27797be14718d8a0bfe7c1a859
Khodr <khodr007@gmail.com>
2019-12-11 10:10:57 -0500

    config files

--- www/js/config.js
+++ www/js/config.js
@@ -5,7 +5,6 @@ fireConfig.$inject = [];
 /* @ngInject */
 function fireConfig () {
 
-<<<<<<< HEAD
     var config = {
         //
         // PreProd (Dev) - ALTHOUGH BELOW IT HAS THE WORD "PROD"
@@ -30,30 +29,4 @@ function fireConfig () {
     if (!firebase.apps.length) {
         firebase.initializeApp(config);
     }
-=======
-	var config = {
-		//
-		// PreProd (Dev) - ALTHOUGH BELOW IT HAS THE WORD "PROD"
-		//
-		apiKey: "AIzaSyAGBV2Zwr0SYKy_J92jceof-OFeeFmm0Gw",
-		authDomain: "opal-prod.firebaseapp.com",
-		databaseURL: "https://opal-prod.firebaseio.com",
-		projectId: "opal-prod",
-		storageBucket: "opal-prod.appspot.com",
-		// messagingSenderId: "476395494069"
-
-		//
-		// PRODUCTION
-		//
-		// apiKey: "AIzaSyCxV6VuVWtKg5wkeReqzQNfRULwkvVXkos",
-		// authDomain: "brilliant-inferno-7679.firebaseapp.com",
-		// databaseURL: "https://brilliant-inferno-7679.firebaseio.com",
-		// storageBucket: "firebase-brilliant-inferno-767.appspot.com",
-		// messagingSenderId: "810896751588"
-	};
-
-	if (!firebase.apps.length) {
-		firebase.initializeApp(config);
-	}
->>>>>>> opal_pre_prod
 }
