4559a89e5aa715c7e1db9217d9353ee25ea98d8b
Yuan Chen <CHYU6010@MUHCAD.MUHCFRD.CA>
2020-03-10 10:20:28 -0400

    Remove changes to config.js

--- www/js/config.js
+++ www/js/config.js
@@ -9,11 +9,11 @@ function fireConfig () {
         //
         // PreProd (Dev) - ALTHOUGH BELOW IT HAS THE WORD "PROD"
         //
-        // apiKey: "AIzaSyAGBV2Zwr0SYKy_J92jceof-OFeeFmm0Gw",
-        // authDomain: "opal-prod.firebaseapp.com",
-        // databaseURL: "https://opal-prod.firebaseio.com",
-        // projectId: "opal-prod",
-        // storageBucket: "opal-prod.appspot.com",
+        apiKey: "AIzaSyAGBV2Zwr0SYKy_J92jceof-OFeeFmm0Gw",
+        authDomain: "opal-prod.firebaseapp.com",
+        databaseURL: "https://opal-prod.firebaseio.com",
+        projectId: "opal-prod",
+        storageBucket: "opal-prod.appspot.com",
         // messagingSenderId: "476395494069"
 
         //
@@ -24,16 +24,6 @@ function fireConfig () {
         // databaseURL: "https://brilliant-inferno-7679.firebaseio.com",
         // storageBucket: "firebase-brilliant-inferno-767.appspot.com",
         // messagingSenderId: "810896751588"
-
-        //
-        // local (Yuan)
-        //
-        apiKey: "AIzaSyBFhzmXGi8lzTqGNe3udnsLWsfJh-YIPfQ",
-        authDomain: "opalstaging-6ecfc.firebaseapp.com",
-        databaseURL: "https://opalstaging-6ecfc.firebaseio.com",
-        projectId: "opalstaging-6ecfc",
-        storageBucket: "opalstaging-6ecfc.appspot.com",
-        messagingSenderId: "1001742009752",
     };
 
     if (!firebase.apps.length) {
