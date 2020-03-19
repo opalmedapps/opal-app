cecd32020407f3bc49b8e60868c92b3a32b1415d
Yuan Chen <19421218+YC000@users.noreply.github.com>
2020-03-19 16:58:25 -0400

    Transition AIO - update link to register for Opal and move to dev firebase (#721)
    * Transition AIO - update link to register for Opal and change to dev firebase

--- www/js/config.js
+++ www/js/config.js
@@ -9,12 +9,22 @@ function fireConfig () {
         //
         // PreProd (Dev) - ALTHOUGH BELOW IT HAS THE WORD "PROD"
         //
-        apiKey: "AIzaSyAGBV2Zwr0SYKy_J92jceof-OFeeFmm0Gw",
-        authDomain: "opal-prod.firebaseapp.com",
-        databaseURL: "https://opal-prod.firebaseio.com",
-        projectId: "opal-prod",
-        storageBucket: "opal-prod.appspot.com",
+        // apiKey: "AIzaSyAGBV2Zwr0SYKy_J92jceof-OFeeFmm0Gw",
+        // authDomain: "opal-prod.firebaseapp.com",
+        // databaseURL: "https://opal-prod.firebaseio.com",
+        // projectId: "opal-prod",
+        // storageBucket: "opal-prod.appspot.com",
         // messagingSenderId: "476395494069"
+
+        //
+        // Dev firebase
+        //
+        apiKey: "AIzaSyAMIDdcQR8EiY9gjj4cgxp6Vu3xwa78Ww8",
+        authDomain: "opal-dev.firebaseapp.com",
+        databaseURL: "https://opal-dev.firebaseio.com",
+        projectId: "opal-dev",
+        storageBucket: "opal-dev.appspot.com",
+        messagingSenderId: "652464215237",
 
         //
         // PRODUCTION
