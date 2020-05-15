56799b1d69e9cf04f7e7ec6d019a14935da0f915
Yuan Chen <19421218+YC000@users.noreply.github.com>
2020-05-15 09:12:08 -0400

    bug fix - fix backslashes showing in treatment team messages and announcements (#736)
    * added a function to strip escaping backslashes in order to fix the issue of backslashes showing in treatment team messages and announcement (for example: prevent \", \', \\, etc from showing)

--- src/js/services/encryptionService.js
+++ src/js/services/encryptionService.js
@@ -3,103 +3,101 @@
 //
 import nacl from "tweetnacl";
 import util from "tweetnacl-util";
-nacl.util = util;
-var myApp=angular.module('MUHCApp');
-/**
-*@ngdoc service
-*@name MUHCApp.service:EncryptionService
-*@requires MUHCApp.service:UserAuthorizationInfo
-*@description Provides an API to encrypt and decrypt objects, arrays, or strings.
-**/
-myApp.service('EncryptionService',function(UserAuthorizationInfo){
 
-	var securityAnswerHash = '';
+nacl.util = util;
+var myApp = angular.module('MUHCApp');
+/**
+ *@ngdoc service
+ *@name MUHCApp.service:EncryptionService
+ *@requires MUHCApp.service:UserAuthorizationInfo
+ *@description Provides an API to encrypt and decrypt objects, arrays, or strings.
+ **/
+myApp.service('EncryptionService', function (UserAuthorizationInfo) {
+
+    var securityAnswerHash = '';
     var encryptionHash = '';
     var tempEncryptionHash = '';
 
     const main_fields = ['UserID', 'Timestamp', 'Code', 'DeviceId'];
 
+    function decryptObject(object, secret) {
+        if (typeof object === 'string') {
+            //grab the nonce
+            var pair = splitValue(object);
+            var decryptedObj = nacl.util.encodeUTF8(nacl.secretbox.open(pair[1], pair[0], secret));
 
-	function decryptObject(object,secret)
-	{
-		if(typeof object ==='string')
-		{
-			//grab the nonce
-			var pair = splitValue(object);
-			return nacl.util.encodeUTF8(nacl.secretbox.open(pair[1], pair[0], secret));
+            return stripEscapingSlashes(decryptedObj);
 
-		}else{
-			for (var key in object)
-			{
-				if (typeof object[key] ==='object')
-				{
-					decryptObject(object[key],secret);
-				} else
-				{
-					if (main_fields.includes(key)) {
+        } else {
+            for (var key in object) {
+                if (typeof object[key] === 'object') {
+                    decryptObject(object[key], secret);
+                } else {
+                    if (main_fields.includes(key)) {
                         object[key] = object[key];
-                    }
-					else
-					{
+                    } else {
                         //grab the nonce
                         pair = splitValue(object[key]);
 
                         var value = nacl.secretbox.open(pair[1], pair[0], secret);
 
-						if(value === null){
+                        if (value === null) {
 
-                            object[key]= '';
+                            object[key] = '';
 
-						}
-						else{
-                            object[key]= nacl.util.encodeUTF8(value);
-						}
-					}
-				}
-			}
-		}
-		return object;
-	}
+                        } else {
+                            var decryptedValue = nacl.util.encodeUTF8(value);
+                            object[key] = stripEscapingSlashes(decryptedValue);
+                        }
+                    }
+                }
+            }
+        }
+        return object;
+    }
 
-    function encryptObject(object,secret, nonce)
-	{
-	 	if (typeof object ==='string'){
-            return nacl.util.encodeBase64( appendUint8Array(nonce, nacl.secretbox(nacl.util.decodeUTF8(object), nonce, secret)));
-	 	}else if(typeof object!=='string'&& typeof object!=='object'){
-	 		object=String(object);
+    function encryptObject(object, secret, nonce) {
+        if (typeof object === 'string') {
             return nacl.util.encodeBase64(appendUint8Array(nonce, nacl.secretbox(nacl.util.decodeUTF8(object), nonce, secret)));
-	 	}else{
-			for (var key in object)
-			{
-				if (typeof object[key]==='object')
-			    {
-			      encryptObject(object[key], secret, nonce);
-			    }
-			    else
-			    {
-			        object[key]=String(object[key]);
+        } else if (typeof object !== 'string' && typeof object !== 'object') {
+            object = String(object);
+            return nacl.util.encodeBase64(appendUint8Array(nonce, nacl.secretbox(nacl.util.decodeUTF8(object), nonce, secret)));
+        } else {
+            for (var key in object) {
+                if (typeof object[key] === 'object') {
+                    encryptObject(object[key], secret, nonce);
+                } else {
+                    object[key] = String(object[key]);
                     object[key] = nacl.util.encodeBase64(appendUint8Array(nonce, nacl.secretbox(nacl.util.decodeUTF8(object[key]), nonce, secret)));
-			    }
-			}
-			return object;
-		}
-	}
+                }
+            }
+            return object;
+        }
+    }
 
     function splitValue(str) {
 
-
-		// Patch for iOS9
-        if(!Uint8Array.prototype.slice)
-        {
-            Uint8Array.prototype.slice = function(a,b){
-                var Uint8ArraySlice = new Uint8Array(this.buffer.slice(a,b));
+        // Patch for iOS9
+        if (!Uint8Array.prototype.slice) {
+            Uint8Array.prototype.slice = function (a, b) {
+                var Uint8ArraySlice = new Uint8Array(this.buffer.slice(a, b));
                 return Uint8ArraySlice;
             }
         }
 
-		var value = nacl.util.decodeBase64(str);
+        var value = nacl.util.decodeBase64(str);
 
         return [value.slice(0, nacl.secretbox.nonceLength), value.slice(nacl.secretbox.nonceLength, value.length)]
+    }
+
+    /**
+     * stripEscapingSlashes
+     * @desc This helper function strips a string of back slashes which serve as escape characters
+     * @param {string} strToBeStripped the string to be stripped of escaping back slashes
+     * @returns {string}
+     */
+    function stripEscapingSlashes(strToBeStripped) {
+        return strToBeStripped.replace(/\\(.)/mg, "$1");
     }
 
     /**
@@ -110,27 +108,26 @@ myApp.service('EncryptionService',function(UserAuthorizationInfo){
      * @param {Uint8Array} buffer2 The second buffer.
      * @return {Uint8Array} The new ArrayBuffer created out of the two.
      */
-    var appendUint8Array = function(buffer1, buffer2) {
+    var appendUint8Array = function (buffer1, buffer2) {
         var tmp = new Uint8Array(buffer1.length + buffer2.length);
         tmp.set(buffer1, 0);
         tmp.set(buffer2, buffer1.length);
         return tmp;
     };
 
-    return{
-		/**
-		*@ngdoc method
-		*@name decryptData
-		*@methodOf MUHCApp.service:EncryptionService
-		*@params {Object} object Object to be decrypted
-		*@description Uses the hashed of the password from the {@link MUHCApp.service:UserAuthorizationInfo  UserAuthorizationInfo} service as key to decrypt object parameter
-    	*@return {Object} Returns decrypted object
-		**/
-		decryptData:function(object)
-		{
-			//Decrypt
-			return decryptObject(object, nacl.util.decodeUTF8(encryptionHash.substring(0, nacl.secretbox.keyLength)));
-		},
+    return {
+        /**
+         *@ngdoc method
+         *@name decryptData
+         *@methodOf MUHCApp.service:EncryptionService
+         *@params {Object} object Object to be decrypted
+         *@description Uses the hashed of the password from the {@link MUHCApp.service:UserAuthorizationInfo  UserAuthorizationInfo} service as key to decrypt object parameter
+         *@return {Object} Returns decrypted object
+         **/
+        decryptData: function (object) {
+            //Decrypt
+            return decryptObject(object, nacl.util.decodeUTF8(encryptionHash.substring(0, nacl.secretbox.keyLength)));
+        },
 
         /**
          *@ngdoc method
@@ -140,39 +137,36 @@ myApp.service('EncryptionService',function(UserAuthorizationInfo){
          *@description Uses the given key as the decryption hash
          *@return {Object} Returns decrypted object
          **/
-        decryptDataWithKey:function(object, key)
-        {
+        decryptDataWithKey: function (object, key) {
             //Decrypt
             return decryptObject(object, nacl.util.decodeUTF8(key.substring(0, nacl.secretbox.keyLength)));
         },
-		/**
-		*@ngdoc method
-		*@name encryptData
-		*@methodOf MUHCApp.service:EncryptionService
-		*@params {Object} object Object to be encrypted
-		*@description Uses the hashed of the password from the {@link MUHCApp.service:UserAuthorizationInfo  UserAuthorizationInfo} service as key to encrypt object parameter
-    	*@return {Object} Returns encrypted object
-		**/
-		encryptData:function(object)
-		{
+        /**
+         *@ngdoc method
+         *@name encryptData
+         *@methodOf MUHCApp.service:EncryptionService
+         *@params {Object} object Object to be encrypted
+         *@description Uses the hashed of the password from the {@link MUHCApp.service:UserAuthorizationInfo  UserAuthorizationInfo} service as key to encrypt object parameter
+         *@return {Object} Returns encrypted object
+         **/
+        encryptData: function (object) {
             var nonce = this.generateNonce();
             return encryptObject(object, nacl.util.decodeUTF8(encryptionHash.substring(0, nacl.secretbox.keyLength)), nonce);
-		},
+        },
 
-		/**
-		*@ngdoc method
-		*@name encryptWithKey
-		*@methodOf MUHCApp.service:EncryptionService
-		*@params {Object} object Object to be encrypted
-		*@params {String} secret Key for encrypting
-		*@description Uses the secret parameter as key to encrypt object parameter
-    	*@return {Object} Returns encrypted object
-		**/
-		encryptWithKey:function(object, secret)
-		{
+        /**
+         *@ngdoc method
+         *@name encryptWithKey
+         *@methodOf MUHCApp.service:EncryptionService
+         *@params {Object} object Object to be encrypted
+         *@params {String} secret Key for encrypting
+         *@description Uses the secret parameter as key to encrypt object parameter
+         *@return {Object} Returns encrypted object
+         **/
+        encryptWithKey: function (object, secret) {
             var nonce = this.generateNonce();
             return encryptObject(object, nacl.util.decodeUTF8(secret.substring(0, nacl.secretbox.keyLength)), nonce);
-		},
+        },
 
         /**
          *@ngdoc method
@@ -181,8 +175,8 @@ myApp.service('EncryptionService',function(UserAuthorizationInfo){
          *@params {String} answer Security answer
          *@description Sets the security answer to be used as the encryption key for all future communication.
          **/
-		setSecurityAns: function (answer) {
-			securityAnswerHash = answer;
+        setSecurityAns: function (answer) {
+            securityAnswerHash = answer;
         },
 
         /**
@@ -192,8 +186,8 @@ myApp.service('EncryptionService',function(UserAuthorizationInfo){
          *@description Uses the secret parameter as key to encrypt object parameter
          *@return {String} Returns hashed security answer
          **/
-		getSecurityAns: function () {
-			return securityAnswerHash;
+        getSecurityAns: function () {
+            return securityAnswerHash;
         },
 
         /**
@@ -204,7 +198,7 @@ myApp.service('EncryptionService',function(UserAuthorizationInfo){
          *@return {String} Returns hashed password
          **/
         hash: function (incoming) {
-         	return CryptoJS.SHA512(incoming).toString();
+            return CryptoJS.SHA512(incoming).toString();
         },
 
         /**
@@ -215,7 +209,10 @@ myApp.service('EncryptionService',function(UserAuthorizationInfo){
          *@return {String} Returns temporary encryption hash
          **/
         generateTempEncryptionHash: function (ssn, answer) {
-            tempEncryptionHash = CryptoJS.PBKDF2(ssn, answer, {keySize: 512/32, iterations: 1000}).toString(CryptoJS.enc.Hex);
+            tempEncryptionHash = CryptoJS.PBKDF2(ssn, answer, {
+                keySize: 512 / 32,
+                iterations: 1000
+            }).toString(CryptoJS.enc.Hex);
 
         },
 
@@ -238,17 +235,19 @@ myApp.service('EncryptionService',function(UserAuthorizationInfo){
          *@return {String} Returns hashed password
          **/
         generateEncryptionHash: function () {
-            encryptionHash = CryptoJS.PBKDF2(UserAuthorizationInfo.getPassword(), securityAnswerHash, {keySize: 512/32, iterations: 1000}).toString(CryptoJS.enc.Hex);
+            encryptionHash = CryptoJS.PBKDF2(UserAuthorizationInfo.getPassword(), securityAnswerHash, {
+                keySize: 512 / 32,
+                iterations: 1000
+            }).toString(CryptoJS.enc.Hex);
 
         },
 
-        generateNonce: function() {
-			return nacl.randomBytes(nacl.secretbox.nonceLength)
-		},
+        generateNonce: function () {
+            return nacl.randomBytes(nacl.secretbox.nonceLength)
+        },
 
-		getTempEncryptionHash: function() {
-        	return tempEncryptionHash;
-		}
-
-	};
+        getTempEncryptionHash: function () {
+            return tempEncryptionHash;
+        }
+    };
 });
