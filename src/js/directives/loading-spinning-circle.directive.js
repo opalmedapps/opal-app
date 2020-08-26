83b8775660d90444992f07e64dcc6dbca284a129
Stacey Beard <staceybeard10@gmail.com>
2020-08-26 11:54:07 -0400

    Add a parameter to the loading spinner directive to change its margin-top height
    - Added a new margin-top parameter to the loading spinner directive
      to override its default margin-top (space above it) of 25vh.
    - Used this override parameter to change the spinner position on
      the security question page for forgot password reset.

--- src/js/directives/loading-spinning-circle.directive.js
+++ src/js/directives/loading-spinning-circle.directive.js
@@ -11,18 +11,19 @@ function LoadingSpinningCircle() {
 		restrict: 'E',
 		scope: {
 			"loadingMessage": "@",
+			// Optional parameter that overrides the default margin-top of 25vh. Example usage: "15vh".
+			"margintop": "@",
 		},
 		replace: false,
 		transclude: false,
 		template: `
             <div align="center">
-                <svg class="progress-circular"  style="margin-top:25vh">
+                <svg class="progress-circular" style="margin-top:25vh" ng-style="margintop && {'margin-top': '{{margintop}}'}">
                     <circle class="progress-circular__primary" cx="50%" cy="50%" r="40%" fill="none" stroke-width="10%"
                             stroke-miterlimit="10"/>
                 </svg>
                 <p style="margin-top: 30px" ng-class="fontSizeDesc">{{ loadingMessage }}</p>
             </div>                      
-            `,
-
+		`,
 	};
 }
