35fab464c25ad131ac0e2c3ef9658533706b1e7f
Kayla O'Sullivan <k.osullivan.steben@hotmail.com>
2021-03-03 16:29:10 -0500

    Improve 3D body look and add beam calculation fns
    - Remove mesh & extrude along path of each slice and cap top/bottom
    - Clean up code and put into functions

--- src/js/controllers/radiotherapy/radiotherapyController.js
+++ src/js/controllers/radiotherapy/radiotherapyController.js
@@ -1,9 +1,11 @@
 // add description see diagnosescontroller
 
 import * as THREE from 'three';
+import { ConvexBufferGeometry } from 'three/examples/jsm/geometries/ConvexGeometry.js';
+import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry.js';
+import { ConvexHull } from 'three/examples/jsm/math/ConvexHull.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import {body} from './testdata.js';
-
+import {body,head, s1, s2, s3, s4, s5, s6 } from './testdata.js';
 
 (function () {
     'use strict';
@@ -26,9 +28,32 @@ import {body} from './testdata.js';
         vm.goToRTPlan = goToRTPlan;
      
 
-        // let camera, scene, renderer;
+        let camera, scene, renderer, group;
+        let smoothMeshx, geometry, tube, mesh, points, points3d;
+        var pointsno = [];
+        var pointsyes = []
+        var meshes = [];
         // let geometry, material, mesh;
 
+        var SAD = 1000;
+        var beams = {
+            1:{
+                isocenter: {x:84.86, y:-61.08,z:-0.5},
+                gantryAngle: 316,
+                X1: 0,
+                X2: 86,
+                Y1: -90,
+                Y2: 80
+            },
+            2:{
+                isocenter: {x:84.86, y:-61.08,z:-0.5},
+                gantryAngle: 136,
+                X1: -103.33,
+                X2: 0,
+                Y1: -90,
+                Y2: 80
+            }
+        }
 
         activate();
 
@@ -36,20 +61,20 @@ import {body} from './testdata.js';
 
         function activate() {      
 
-            vm.rtPlans = [
-                {
-                    name: "Test brainstem 3D"
-                }
+            // vm.rtPlans = [
+            //     {
+            //         name: "Test brainstem 3D"
+            //     }
 
-            ]
+            // ]
          
 
             
             //grab the language
             vm.language = UserPreferences.getLanguage();
 
-            // initCube();
-            initBody();
+            renderElements();
+            
         }
 
         // Determines whether or not to show the date header in the view. Announcements are grouped by day.
@@ -74,123 +99,103 @@ import {body} from './testdata.js';
 
 
         }
-        
-        function initCube() {
 
-            var scene = new THREE.Scene();
-            var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-            var renderer = new THREE.WebGLRenderer();
-            renderer.setSize( window.innerWidth, window.innerHeight );
-            document.getElementById("holder").appendChild( renderer.domElement );
+        function renderElements(){
+           
+            scene = new THREE.Scene();
+            scene.background = new THREE.Color(0xf1f1f1);
 
-            var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-            var cube = new THREE.Mesh( geometry, material );
-            scene.add( cube );
+            group = new THREE.Group();
 
-            camera.position.z = 5;
-
-            const controls = new OrbitControls( camera, renderer.domElement );
-
-            var animate = function () {
-                requestAnimationFrame( animate );
-              
-                renderer.render( scene, camera );
-              };
-              
-              animate();
+            camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 1, 1000 );
+            camera.position.y = -300
+            camera.position.z = 100
             
-        }
-
-        function initBody(){
-            var scene = new THREE.Scene();
-            scene.background = new THREE.Color(0x000000);
-
-            var camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, 1, 500 );
-            camera.position.z = 100;
 
             const light = new THREE.PointLight(0xffffff, 1.5);
-            light.position.set(1000,1000,2000);
+            light.position.set(0,0,2000);
             scene.add(light);
 
-            const geometry = new THREE.BufferGeometry();
-            const vertices = new Float32Array(body);
-            geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-        
+            const light2 = new THREE.PointLight(0xffffff, 1.5);
+            light2.position.set(0,0,-2000);
+            scene.add(light2);
 
-            const material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe:true});
-            const mesh = new THREE.Mesh(geometry, material);
-            scene.add(mesh);
+            const light3 = new THREE.PointLight(0xffffff, 1.5);
+            light3.position.set(1000,-1000,0);
+            scene.add(light3);
 
+            const light4 = new THREE.PointLight(0xffffff, 1.5);
+            light4.position.set(-1000,1000,0);
+            scene.add(light4);
 
-
-            const geometry2 = new THREE.SphereGeometry( 5);
-            const material2 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-            const sphere = new THREE.Mesh( geometry2, material2 );
-            sphere.position.set(84.86,-61.08,-0.5)
-            // scene.add( sphere );
-
-            // const geometry2 = new THREE.SphereGeometry( 5);
-            // const material2 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-            const sphere2 = new THREE.Mesh( geometry2, material2 );
-            sphere2.position.set(-609.84,-780.38,-0.5)
-            scene.add( sphere2 );
-
-            const sphere3 = new THREE.Mesh( geometry2, material2 );
-            sphere3.position.set(145.9,-120.7,-0.5)
-            // scene.add( sphere3 );
-
-            const sphere4 = new THREE.Mesh( geometry2, material2 );
-            sphere4.position.set(84.86,-61.08,-90.5)
-            scene.add( sphere4 );
-
-            const sphere5 = new THREE.Mesh( geometry2, material2 );
-            sphere5.position.set(84.86,-61.08,70.5)
-            scene.add( sphere5 );
-
-            const sphere6 = new THREE.Mesh( geometry2, material2 );
-            sphere6.position.set(145.9,-120.7,70.5)
-            scene.add( sphere6 );
-
-            const sphere7 = new THREE.Mesh( geometry2, material2 );
-            sphere7.position.set(145.9,-120.7,-90.5)
-            scene.add( sphere7 );
-            // const colx1 
-
-            const points = [];
-            // points.push( new THREE.Vector3(84.86,-61.08,-0.5));
-            // points.push( new THREE.Vector3(-609.84,-780.38,-0.5));
-            // points.push( new THREE.Vector3(145.9,-120.7,-0.5));
-            // points.push( new THREE.Vector3(-609.84,-780.38,-0.5));
-            points.push( new THREE.Vector3(84.86,-61.08,-90.5));
-            points.push( new THREE.Vector3(-609.84,-780.38,-0.5));
-            points.push( new THREE.Vector3(84.86,-61.08,70.5));
-            points.push( new THREE.Vector3(-609.84,-780.38,-0.5));
-            points.push( new THREE.Vector3(145.9,-120.7,70.5));
-            points.push( new THREE.Vector3(-609.84,-780.38,-0.5));
-            points.push( new THREE.Vector3(145.9,-120.7,-90.5));
-            points.push( new THREE.Vector3(145.9,-120.7,70.5));
-            points.push( new THREE.Vector3(84.86,-61.08,70.5));
-            points.push( new THREE.Vector3(84.86,-61.08,-90.5));
-            points.push( new THREE.Vector3(145.9,-120.7,-90.5));
-            const material3 = new THREE.LineBasicMaterial( { color: 0xff0000, linewidth: 4 } );
-            const geometry3 = new THREE.BufferGeometry().setFromPoints( points );
-            const line = new THREE.Line( geometry3, material3 );
-            scene.add(line)
+            // const axesHelper = new THREE.AxesHelper( 300 );
+            // scene.add( axesHelper );
+          
+            renderBody();
+            renderBeam(beams[1])
+            renderBeam(beams[2])
 
 
-            var renderer = new THREE.WebGLRenderer({antialias: true});
+        //     let hull = new ConvexHull().setFromObject(meshes[0])
+        //     let hull1 = new ConvexHull().setFromObject(meshes[1])
+        //     points3d.forEach(function(vertex){
+        //         if (hull.containsPoint(vertex) || hull1.containsPoint(vertex)){
+        //             pointsyes.push(vertex)
+        //         } else {
+        //             pointsno.push(vertex)
+        //         }
+        //     } )
+
+        //     const material = new THREE.LineBasicMaterial( {
+        //         color: 0xff6666,})
+                
+        // const geometry = new THREE.BufferGeometry().setFromPoints( pointsyes );
+        // const lines = new THREE.Line(geometry, material)
+        // group.add(lines)
+
+            
+       
+            // var currZ = pointsyes[0].z
+            // var newZ = currZ
+
+            // var array = []
+            // var geo;
+            // var shape = new THREE.Shape();
+            // shape.moveTo(0,0);
+            // shape.lineTo(1.5,1)
+            // shape.lineTo(1.5,-1)
+            // shape.lineTo( -1.5,-1);
+            // shape.lineTo(-1.5,1)
+            // // shape.lineTo( 0, 0 );
+    
+            // pointsyes.forEach(function(vert){
+            //     newZ = vert.z
+            //     if (newZ != currZ){
+            //             var sampleClosedSpline = new THREE.CatmullRomCurve3( array);
+            //             var tube = new THREE.TubeBufferGeometry( sampleClosedSpline, 64, 2, 4);
+            //             geo = new THREE.ExtrudeBufferGeometry(shape, {extrudePath:sampleClosedSpline,steps:500})
+            //             mesh = new THREE.Mesh(geo, new THREE.MeshPhongMaterial({color:0x4C0B5F, side:THREE.DoubleSide, shininess:40})) //0x04B45F
+            //             currZ = newZ;
+            //             group.add(mesh)
+                        
+            //             array = [];
+            //         }
+    
+            //        array.push(vert)
+                
+            // })
+
+            scene.add(group)
+
+            renderer = new THREE.WebGLRenderer({antialias: true});
             renderer.setPixelRatio(window.devicePixelRatio)
             renderer.setSize( window.innerWidth, window.innerHeight );
             document.getElementById("holder").appendChild( renderer.domElement );
 
 
 
-            
-
             const controls = new OrbitControls( camera, renderer.domElement );
-
 
             var animate = function () {
                 requestAnimationFrame( animate );
@@ -199,12 +204,158 @@ import {body} from './testdata.js';
               };
               
               animate();
-            
-
         }
-      
 
+        function renderBody(){
+            
+            let colour = 0x29293d//3104B4//0B4C5F//// 0x//0xa29093 //0x669999
+            geometry = new THREE.BufferGeometry();
+            const vertices = new Float32Array(body);
+            geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+            const positionAttribute = geometry.getAttribute( 'position' );
+            points3d = []
+            var hole = []
+            var total = []
+
+            for ( let i = 0; i < positionAttribute.count; i += 3 ) {
+                const vertex = new THREE.Vector3();
+                vertex.fromBufferAttribute( positionAttribute, i );
+                points3d.push( vertex );
+                total.push(vertex)
+                hole.push(new THREE.Vector3(vertex.x*.9, vertex.y*.9, vertex.z))
+                total.push(new THREE.Vector3(vertex.x*.9, vertex.y*.9, vertex.z))
+          
+            }
+
+
+            var slice1 = []
+
+            for (let i = 0; i < s1.length; i+=3){
+                slice1.push(new THREE.Vector2(s1[i], s1[i+1]))
+            }
+            const s1shape = new THREE.Shape(slice1)
+            const geo1 = new THREE.ShapeBufferGeometry(s1shape)
+
+            const mesh1 = new THREE.Mesh( geo1, new THREE.MeshPhongMaterial( {color:colour, side:THREE.DoubleSide,shininess:30}))
+            mesh1.position.z = s1[2]
+            group.add(mesh1)
+
+            var slice6 = []
+
+            for (let i = 0; i < s6.length; i+=3){
+                slice6.push(new THREE.Vector2(s6[i], s6[i+1]))
+            }
+            const s6shape = new THREE.Shape(slice6)
+            const geo6 = new THREE.ShapeBufferGeometry(s6shape)
+
+            const mesh6 = new THREE.Mesh( geo6, new THREE.MeshPhongMaterial( {color:colour, side:THREE.DoubleSide,shininess:30}))
+            mesh6.position.z = s6[2]
+            group.add(mesh6)
+
+            var currZ = body[2];
+            var newZ = body[2]
+            var array = [];
+
+            var shape, geo, mesh, hole;
+
+
+            shape = new THREE.Shape();
+            shape.moveTo(0,0);
+            shape.lineTo(1.5,0)
+            shape.lineTo(1.5,1)
+            shape.lineTo( -1.5, 1);
+            shape.lineTo(-1.5,0)
+            shape.lineTo( 0, 0 );
+
+            for (let i = 0; i < body.length; i+=18){
+                newZ = body[i+2]
+                if (newZ != currZ){ 
+
+                    var sampleClosedSpline = new THREE.CatmullRomCurve3( array, true);
+                    var tube = new THREE.TubeBufferGeometry( sampleClosedSpline, 64, 2, 4, true);
+                    geo = new THREE.ExtrudeBufferGeometry(shape, {extrudePath:sampleClosedSpline,steps:500})
+                    mesh = new THREE.Mesh(geo, new THREE.MeshPhongMaterial({color:colour, side:THREE.DoubleSide, shininess:40})) //LineBasicMaterial())//
+                    currZ = newZ;
+                    group.add(mesh)
+                    
+                    array = [];
+                }
+
+               array.push(new THREE.Vector3(body[i], body[i+1], body[i+2]))
+            }
     }
-               
+
+
+        function renderBeam(beam){
+            let xi = beam.isocenter.x;
+            let yi = beam.isocenter.y;
+            let zi = beam.isocenter.z;
+
+            let angle = beam.gantryAngle * Math.PI / 180;
+
+            let beamSource = [xi + SAD*Math.sin(angle), yi - SAD*Math.cos(angle), zi];
+
+            let c1 = [xi + beam.X1*Math.cos(angle), yi + beam.X1*Math.sin(angle), zi + beam.Y2]
+            let c2 = [xi + beam.X2*Math.cos(angle), yi + beam.X2*Math.sin(angle), zi + beam.Y2]
+            let c3 = [xi + beam.X2*Math.cos(angle), yi + beam.X2*Math.sin(angle), zi + beam.Y1]
+            let c4 = [xi + beam.X1*Math.cos(angle), yi + beam.X1*Math.sin(angle), zi + beam.Y1]
+
+            var beamPoints = [];
+            beamPoints.push(beamSource)
+            beamPoints.push(c1)
+            beamPoints.push(c2)
+            beamPoints.push(c3)
+            beamPoints.push(c4)
+
+            // beamPoints.push(xi + SAD*Math.sin(angle), yi - SAD*Math.cos(angle), zi)
+
+            // beamPoints.push(xi + beam.X1*Math.cos(angle), yi + beam.X1*Math.sin(angle), zi + beam.Y2)
+            // beamPoints.push(xi + beam.X2*Math.cos(angle), yi + beam.X2*Math.sin(angle), zi + beam.Y2)
+            // beamPoints.push(xi + beam.X2*Math.cos(angle), yi + beam.X2*Math.sin(angle), zi + beam.Y1)
+            // beamPoints.push(xi + beam.X1*Math.cos(angle), yi + beam.X1*Math.sin(angle), zi + beam.Y1)
+
+            let pointsGeometry = new THREE.BufferGeometry();
+            pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(beamPoints, 3));
+
+            const geometry2 = new THREE.SphereGeometry( 5);
+            const material2 = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+            beamPoints.forEach(function(point){
+                let sphere = new THREE.Mesh(geometry2, material2);
+                sphere.position.set(point[0], point[1], point[2])
+                group.add(sphere)                
+            });
+
+
+        
+            
+            const points = [];
+            points.push( new THREE.Vector3(c1[0],c1[1],c1[2]));
+            points.push( new THREE.Vector3(beamSource[0],beamSource[1],beamSource[2]));
+            points.push( new THREE.Vector3(c2[0],c2[1],c2[2]));
+            points.push( new THREE.Vector3(beamSource[0],beamSource[1],beamSource[2]));
+            points.push( new THREE.Vector3(c3[0],c3[1],c3[2]));
+            points.push( new THREE.Vector3(beamSource[0],beamSource[1],beamSource[2]));
+            points.push( new THREE.Vector3(c4[0],c4[1],c4[2]));
+            points.push( new THREE.Vector3(c1[0],c1[1],c1[2]));
+            points.push( new THREE.Vector3(c2[0],c2[1],c2[2]));
+            points.push( new THREE.Vector3(c3[0],c3[1],c3[2]));
+            points.push( new THREE.Vector3(c4[0],c4[1],c4[2]));
+            const lineMaterial = new THREE.LineBasicMaterial( { color: 0xff0000, linewidth: 4 } );
+            const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
+            const lines = new THREE.Line( lineGeometry, lineMaterial );
+            group.add(lines)
+
+            lineGeometry.computeVertexNormals();
+
+            const meshGeometry3 = new ConvexGeometry( points );
+            const smoothMaterialx = new THREE.MeshMatcapMaterial({color: 0xff0000, side: THREE.DoubleSide, transparent: true, opacity: 0.2});
+            smoothMeshx = new THREE.Mesh( meshGeometry3, smoothMaterialx)
+            group.add(smoothMeshx)
+
+            meshes.push(smoothMeshx)
+       
+        }
+    }
 
 })();
