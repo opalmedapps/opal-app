d8f74ca93a1e82d0eb79daf7e4959c1ee7569b28
Kayla O'Sullivan <k.osullivan.steben@hotmail.com>
2021-05-12 14:55:06 -0400

    Add functionality for multislice image scans

--- src/js/controllers/imaging/individualImagingController.js
+++ src/js/controllers/imaging/individualImagingController.js
@@ -1,4 +1,3 @@
-
 (function () {
     'use strict';
 
@@ -12,14 +11,14 @@
 
     function individualImagingController($filter, $scope, $timeout, NavigatorParameters, Radiotherapy, UserPreferences) {
         var vm = this;
-        
+        vm.loading = true
         vm.images = [];
         vm.language = '';
         vm.noImage = false;
         vm.showHeader = showHeader;
+        vm.getSrc = getSrc
         // vm.openImage = openImage;
         vm.slice = 0;
-        vm.uploadDICOM = uploadDICOM;
         // vm.uploadRT = uploadRT;
         vm.slicePath = '';
         vm.showDetails = false;
@@ -33,10 +32,12 @@
         vm.date
         vm.modality
         vm.file;
+        vm.files
         vm.filePath = '';
         vm.pixelData = [];
         vm.url = '';
         vm.loaded = false;
+        vm.index = 1
 
         vm.tags = [];
 
@@ -60,12 +61,16 @@
             
         Radiotherapy.requestImgDicomContent(7)
         .then(function (file) {
-            vm.file = 'data:image/jpg;base64,' + file.img.slice(1,-1);  
+            file = JSON.parse(file)
+            vm.files = file.img
+            vm.multiImage = vm.files.length > 1
+
             vm.modality =file.modality
             vm.date =  $filter('formatDateDicom')(file.date)
 
             console.log(file)
             vm.loaded = true;
+            vm.loading=false
             // loadSingleDicom(file);
             
 
@@ -86,6 +91,35 @@
             //grab the language
             vm.language = UserPreferences.getLanguage();
 
+            vm.index = 1
+
+            var element = document.getElementById("imgs");
+            var startY = 0;
+            var currentY = 0;
+
+            element.addEventListener("touchstart", function(e){
+                startY = e.touches[0].clientY;
+                e.preventDefault(); // prevent page from scrolling 
+            })
+       
+            element.addEventListener("touchmove", function(e){
+
+                currentY = e.touches[0].clientY;
+            
+                if (Math.abs(currentY-startY)>10){
+                    var change = Math.round((currentY - startY)/10)
+
+                    if ((change+vm.index) < 1 ) vm.index = 1
+                    else if ((change+vm.index) > vm.files.length) vm.index = vm.files.length
+                    else vm.index = vm.index + change
+
+                    $scope.$apply();
+                    startY = currentY
+                }
+       
+                e.preventDefault(); // prevent page from scrolling 
+                
+            })
         }
 
         // Determines whether or not to show the date header in the view. Announcements are grouped by day.
@@ -98,7 +132,9 @@
         }
 
     
-
+        function getSrc(index){
+            return "data:image/jpg;base64," + vm.files[index]
+        }
         
   
 
