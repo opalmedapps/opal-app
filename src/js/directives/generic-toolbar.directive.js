8fd7e508593511ac941dab38a9980c6a21d46fe5
David Gagne <david.gagne@muhc.mcgill.ca>
2022-01-17 19:26:14 -0500

    Fix navigation for screen readers on the init page

--- src/js/directives/generic-toolbar.directive.js
+++ src/js/directives/generic-toolbar.directive.js
@@ -26,7 +26,7 @@
             },
             template: `
                 <ons-toolbar fixed-style>
-                    <div class="left" ng-transclude="leftContentSlot"><ons-back-button ng-hide="backButton === false">{{"BACK"|translate}}</ons-back-button></div>
+                    <div class="left" ng-transclude="leftContentSlot"><ons-back-button role="button" ng-hide="backButton === false">{{"BACK"|translate}}</ons-back-button></div>
                     <div class="center overflow-text-ellipsis" ng-class="styleClass">{{title}}</div>
                     <div class="right" ng-transclude="rightContentSlot"></div>
                 </ons-toolbar>
