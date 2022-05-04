cd0169d4029b1e3b32da28ec06889e039cc5bd32
Stacey Beard <staceybeard10@gmail.com>
2022-05-04 15:05:06 +0000

    QSCCD-46: Add a patient-data-handler to Lab Results (!902)
    Made changes to lab results to support data access through a patient-data-handler. As a consequence, refreshing the labs page now only gets new changes instead of re-downloading everything.
    
    * Added requests PatientTestDates and PatientTestTypes to the interface in UpdateUI to make these requests available through the patient-data-handler (see listener MR for details).
    * Added a patient-data-handler to `test-results.html`.
    * Refactored the main labs controller and service to use the normal function layout instead of JS classes. This was necessary due to many `TypeError: attempted to set private field on non-instance` errors when trying to access this controller and service via the patient-data-handler and UpdateUI.
    * Refactored the UpdateUI service to match the JohnPapa style guide.
    * Replaced a redundant `showDateHeader` function with the `showHeader` filter, and slightly modified the filter to be able to process an array of plain `Dates` (without a `sortBy` key).
    
    See merge request opalmedapps/opal-app#902

--- src/js/filters/showHeader.filter.js
+++ src/js/filters/showHeader.filter.js
@@ -10,15 +10,17 @@
      * @author David Gagne
      * @param {array} sourceArray Array from where values to be compared are coming from.
      * @param {int} index Index of the current value to be evaluated.
-     * @param {String} sortBy Name of the key used to compare two values from the sourceArray.
+     * @param {String} [sortBy] (Optional) Name of the key used to compare two values from the sourceArray.
+     *                          If not provided, the object itself from sourceArray is used (should be a date or convertable to Date).
      * @date 2021-12-14
      */
     function ShowHeader() {
         return function(sourceArray, index, sortBy) {
             if (index === 0) return true;
-            var current = (new Date(sourceArray[index][sortBy])).setHours(0,0,0,0);
-            var previous = (new Date(sourceArray[index-1][sortBy])).setHours(0,0,0,0);
-    
+            let current = sortBy ? new Date(sourceArray[index][sortBy]) : new Date(sourceArray[index]);
+            let previous = sortBy ? new Date(sourceArray[index-1][sortBy]) : new Date(sourceArray[index-1]);
+            current = current.setHours(0,0,0,0);
+            previous = previous.setHours(0,0,0,0);
             return current !== previous;
         }
     }
