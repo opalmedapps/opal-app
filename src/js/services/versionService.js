f4ade0b03b082925bab6e61f683c037950f7488f
Limin liu <limin.liu@muhc.mcgill.ca>
2022-08-17 14:38:26 +0000

    feat: Add what's new popup widget (QSCCD-249)
    * bug deps fix max version whats new issue
    
    * bug deps remove parent widget scrollbar
    
    * docs deps add comment to explain lastVersion definition
    
    * feat deps use angular $http instead of https
    feat deps use <ul><li> instead of <p>
    
    * feat deps add a new css file for info modal
    
    * feat deps refactored the code
    feat deps improved the sorting
    
    * feat deps fix the scrollbar for ons-modal
    
    * feat deps refactor the code
    
    * feat deps refactor the code for MR
    
    * feat deps fix reading remote json file issue
    feat deps improve the language of the modal
    
    * feat deps refactor the code
    
    * feat deps refactor the code
    
    See merge request opalmedapps/qplus!916

---
+++ src/js/services/versionService.js
@@ -0,0 +1,126 @@
+/*
+ * Author Limin 2022-07-05
+ * Refactored by Limin on 2022-07-07
+ */
+
+/**
+ * @ngdoc service
+ * @name MUHCApp.service:Version
+ * @requires $filter
+ * @requires $q
+ * @requires MUHCApp.service:RequestToServer
+ * @description API service used for app version information.
+ **/
+(function() {
+    'use strict';
+
+    angular
+        .module('MUHCApp')
+        .factory('Version', Version);
+
+    Version.$inject = ['$filter','$q','RequestToServer', 'Constants', '$http'];
+
+    function Version($filter, $q, RequestToServer, Constants, $http) {
+
+        /**
+         * @ngdoc property
+         * @name MUHCApp.service.#Version
+         * @propertyOf MUHCApp.service:Version
+         * @description Initializing array that represents all the information for Version.
+         *              This array is passed to appropriate controllers.
+         */
+
+        const version_url = 'https://app.opalmedapps.ca/versions.json';
+
+        let service =  {
+            getVersionUpdates: getVersionUpdates,
+            currentVersion: currentVersion,
+        };
+
+        return service;
+
+
+        /**
+         * @ngdoc method
+         * @name getVersionUpdates
+         * @methodOf MUHCApp.service:Version
+         * @description get the version update information.
+         **/
+        async function getVersionUpdates(lastVersion, currentVersion, language) {
+            var r = $q.defer();
+
+            await $http.get(version_url).then(function successCallback(response) {
+                try {
+                    let versions = response.data;
+                    versions.sort(function(v1, v2){return versionCompare(v2.VERSION, v1.VERSION)});
+                    let updates = [];
+                    versions.forEach(function(value) {
+                        if (versionCompare(value.VERSION, lastVersion) === 1 &&
+                            versionCompare(value.VERSION, currentVersion) !== 1 &&
+                            value.DESCRIPTION_EN)
+                        {
+                            let infoData = {};
+                            let description = language == 'EN' ? value.DESCRIPTION_EN : value.DESCRIPTION_FR;
+                            infoData.title = value.VERSION;
+                            infoData.content = description;
+                            updates.push(infoData);
+                        }
+                    });
+                    r.resolve(updates);
+                } catch (error) {
+                    console.error(error.message);
+                    r.reject(error.message);
+                };
+            }, function errorCallback(response) {
+                console.log(response);
+                r.reject(response);
+            });
+
+            return r.promise;
+        }
+
+        /**
+         * @name versionCompare
+         * @desc compare version1 and version2
+         *       return 1 if v1 is greater than v2
+         *       return 0 if v1 = v2
+         *       return -1 if v1 < v2
+         */
+        function versionCompare(v1, v2)
+        {
+            var a = v1.split('.');
+            var b = v2.split('.');
+
+            for (var i = 0; i < a.length; ++i) {
+                a[i] = Number(a[i]);
+            }
+            for (var i = 0; i < b.length; ++i) {
+                b[i] = Number(b[i]);
+            }
+            if (a.length == 2) {
+                a[2] = 0;
+            }
+
+            if (a[0] > b[0]) return 1;
+            if (a[0] < b[0]) return -1;
+
+            if (a[1] > b[1]) return 1;
+            if (a[1] < b[1]) return -1;
+
+            if (a[2] > b[2]) return 1;
+            if (a[2] < b[2]) return -1;
+
+            return 0;
+        }
+
+        /**
+         * @ngdoc method
+         * @name currentVersion
+         * @methodOf MUHCApp.service:Version
+         * @description get the current version of the app.
+         **/
+        function currentVersion() {
+            return Constants.version();
+        }
+    }
+})();
