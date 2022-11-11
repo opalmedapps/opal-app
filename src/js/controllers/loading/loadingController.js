e7a5d22da229baf5952e90dc85913025ed97b1ce
David Gagne <davidgagne85@gmail.com>
2022-11-11 16:24:16 +0000

    QSCCD-658: Added profile icon to home page.
    * Adjustments following code review
    
    * Fixed race condition and refactor old code
    
    * Added profile icon to home page.
    
    Still need to fix race issue flikering display and css for tablettes
    
    See merge request opalmedapps/opal-app#943

--- src/js/controllers/loading/loadingController.js
+++ src/js/controllers/loading/loadingController.js
@@ -22,22 +22,19 @@
 
             var userAuthorizationInfo = UserAuthorizationInfo.getUserAuthData();
 
-            if(!userAuthorizationInfo) {
-                $state.go('init');
-            }
+            if (!userAuthorizationInfo) $state.go('init');
 
             loadingmodal.show();
 
-            Patient.initPatient().then(UpdateUI.init).then(function() {
+            Patient.initPatient().then(UpdateUI.init).then(async () => {
                 $state.go('Home');
                 RequestToServer.sendRequestWithResponse('AccountChange', {NewValue: UserPreferences.getLanguage(), FieldToChange: 'Language'});
 
                 //fetch all the tab metadata TODO: add the fetching of all the other data
                 MetaData.init();
 
-                // Init the profile selector and load the patient list.
-                ProfileSelector.init();
-
+                // Init the profile selector and load the patient list. Needs to be await to prevent profile icon from flickering at login.
+                await ProfileSelector.init();
                 loadingmodal.hide();
                 clearTimeout(timeOut);
 
