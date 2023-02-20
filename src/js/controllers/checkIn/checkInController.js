2d7edc22a41814fa31889550b9e1f98ab879bb9b
Limin liu <limin.liu@muhc.mcgill.ca>
2023-02-20 12:28:26 +0000

    feat: add click event for check-in button (QSCCD-695)
    * feat deps refactor the code
    
    * feat deps add click event for check-in button
    
    See merge request opalmedapps/qplus!968

--- src/js/controllers/checkIn/checkInController.js
+++ src/js/controllers/checkIn/checkInController.js
@@ -56,6 +56,7 @@
 
         vm.goToAppointment = goToAppointment;
         vm.HasMeaningfulAlias = HasMeaningfulAlias;
+        vm.checkInAppointments = checkInAppointments;
 
         activate();
 
@@ -122,8 +123,7 @@
             return HasNonCheckinable;
         }
 
-        /**
-         *
+         /*
          * @param PatientSerNum
          * @return {void}
          * @description Checks if in the list of Appointments,for the target patient
@@ -133,8 +133,29 @@
                 app.loading = true;
             })
 
-            //TODO check-in apps for the target patient
-            const response = await CheckInService.attemptCheckin(PatientSerNum);
+            try {
+                const response = await CheckInService.attemptCheckin(PatientSerNum);
+                if(response === "NOT_ALLOWED"){
+                    Toast.showToast({
+                        message: $filter('translate')("NOT_ALLOWED"),
+                    });
+                    vm.alert.type = Params.alertTypeWarning;
+                    vm.checkInMessage = "CHECKIN_IN_HOSPITAL_ONLY";
+                } else if (response === "SUCCESS") {
+                    vm.alert.type = Params.alertTypeSuccess;
+                    vm.checkInMessage = "CHECKED_IN";
+                    vm.apps = CheckInService.getCheckInApps();
+                } else {
+                    vm.alert.type = Params.alertTypeDanger;
+                    vm.checkInMessage = "CHECKIN_ERROR";
+                    vm.apps = CheckInService.getCheckInApps();
+                    vm.error = "ERROR";
+                    displayError();
+                }
+            } catch (error) {
+                console.log(error);
+                displayError();
+            }
 
             $timeout(() => {
                 let allCheckedIn = true;
