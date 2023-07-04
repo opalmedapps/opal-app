4253b97cca59c13423e60a72d555f553add4834f
Shifeng Chen <shifeng.chen@muhc.mcgill.ca>
2023-07-04 14:54:59 +0000

    fix: update the page and controller based on the response data from listener
    See merge request opalmedapps/opal-app#1003

--- src/js/models/settings/SecurityQuestion.js
+++ src/js/models/settings/SecurityQuestion.js
@@ -2,10 +2,8 @@
  * SecurityQuestion class to model the security question from the back-end
  */
 export class SecurityQuestion {
-    constructor({Active, QuestionText_EN, QuestionText_FR, SecurityQuestionSerNum}) {
-        this.active = Active === "1";
-        this.questionText_EN = QuestionText_EN;
-        this.questionText_FR = QuestionText_FR;
-        this.securityQuestionSerNum = Number(SecurityQuestionSerNum);
+    constructor({title_en, title_fr}) {
+        this.questionText_EN = title_en;
+        this.questionText_FR = title_fr;
     }
 }
