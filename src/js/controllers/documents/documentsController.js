fb1bd237336304e31f76fe8f961735ce4d4c6be8
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2024-02-21 14:38:49 -0500

    feat: implicitly mark notifications as read

--- src/js/controllers/documents/documentsController.js
+++ src/js/controllers/documents/documentsController.js
@@ -9,10 +9,12 @@
         .module('MUHCApp')
         .controller('DocumentsController', DocumentsController);
 
-    DocumentsController.$inject = ['Documents', '$filter', 'NavigatorParameters', 'Permissions', 'Logger', '$timeout'];
+    DocumentsController.$inject = [
+        'Documents', '$filter', 'NavigatorParameters', 'Permissions', 'Logger', '$timeout', 'Notifications',
+    ];
 
     /* @ngInject */
-    function DocumentsController(Documents, $filter, NavigatorParameters, Permissions, Logger, $timeout) {
+    function DocumentsController(Documents, $filter, NavigatorParameters, Permissions, Logger, $timeout, Notifications) {
         var vm = this;
         vm.noDocuments = true;
         vm.documents = [];
@@ -51,9 +53,32 @@
             {
                 doc.ReadStatus ='1';
                 Documents.readDocument(doc.DocumentSerNum);
+                // Mark corresponding notification as read
+                markNotificationAsRead(doc.DocumentSerNum);
             }
             NavigatorParameters.setParameters({'navigatorName':'personalNavigator', 'Post':doc});
             personalNavigator.pushPage('./views/personal/documents/individual-document.html');
+        }
+
+        /*********************************/
+        /******* PRIVATE FUNCTIONS *******/
+        /*********************************/
+
+        /**
+         * @name markNotificationAsRead
+         * @desc Mark corresponding "Document" or "UpdDocument" notifications as read
+         */
+        function markNotificationAsRead(serNum) {
+            const notifications = Notifications.getUserNotifications();
+                if (Array.isArray(notifications) && notifications.length)
+                {
+                    notifications.forEach(
+                        (notif, index) => {
+                            if (notif.RefTableRowSerNum === serNum
+                                && (notif.NotificationType === "Document" || notif.NotificationType === "UpdDocument"))  //TODO: constants for UpdDocument, Document, and NewLabResult
+                                Notifications.readNotification(index, notif);
+                        });
+                }
         }
     }
 })();
