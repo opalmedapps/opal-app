e87624b264c8cf572ff32e6da71a454af8b7376b
Stacey Beard <staceybeard10@gmail.com>
2024-02-27 15:22:10 +0000

    fix: reassign Dev and Staging Firebase projects
    Reassigned the "opal-dev" Firebase project to Opal Dev, and created a new Firebase project for Staging. This was done both as a quick fix for the dev project (which exceeded the quota for free usage), and to improve the confusing name assignments of the Firebase projects.
    
    See merge request opalmedapps/opal-app#1135

--- env/staging/opal.config.js
+++ env/staging/opal.config.js
@@ -17,13 +17,13 @@ const config = {
         "ANDROID_DEBUGGABLE": true,
     },
     firebase: {
-        apiKey: "AIzaSyAMIDdcQR8EiY9gjj4cgxp6Vu3xwa78Ww8",
-        authDomain: "opal-dev.firebaseapp.com",
-        databaseURL: "https://opal-dev.firebaseio.com",
-        projectId: "opal-dev",
-        storageBucket: "opal-dev.appspot.com",
-        messagingSenderId: "652464215237",
-        appId: "1:652464215237:web:0254157de7cc10f8f94cac",
+        apiKey: "AIzaSyAr_Lrr4xQyF-zmSEf3c2iHkYN87eISV-8",
+        authDomain: "opal-5c4f26.firebaseapp.com",
+        databaseURL: "https://opal-5c4f26-default-rtdb.firebaseio.com",
+        projectId: "opal-5c4f26",
+        storageBucket: "opal-5c4f26.appspot.com",
+        messagingSenderId: "908828898055",
+        appId: "1:908828898055:web:6333f319543349c1a10ba2"
     },
 };
 
