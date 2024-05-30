c1ac406eda3bd9d9ff5f66386ee25640fd500f1f
Stacey Beard <staceybeard10@gmail.com>
2024-05-30 17:50:08 +0000

    fix: grey out the Chart toolbar icon when unselected
    Updated the Chart tab button (the colour-coded patient icon with initials) so that its behaviour is synchronized with the onsen tab bar. Now, the icon is grey when unselected, and in color when selected, which is consistent with the other buttons on the bar. This makes it easier for users to understand at a glance which tab they're viewing.
    
    See merge request opalmedapps/opal-app#1182

--- src/js/directives/profile-icon.directive.js
+++ src/js/directives/profile-icon.directive.js
@@ -1,4 +1,3 @@
-
 (function () {
     'use strict';
 
@@ -18,11 +17,14 @@
     {
         return {
             restrict: 'E',
-            scope: true,
+            scope: {
+                // Optional boolean: if true, the profile icon syncs with its parent ons tab (colour when selected, grey when unselected)
+                "syncWithOnsToolbar": "<?",
+            },
             template: `
                 <span class="tab-icon profile-icon" ng-style="profileColor">{{patientInitials}}</span>
             `,
-            link: function (scope) {
+            link: function (scope, element) {
                 // Needed to force an update to the icon on the tab bar in certain cases
                 // For example when clicking back from a Questionnaire notification that changes profiles
                 ProfileSelector.attachToObserver(refreshDisplay);
@@ -30,9 +32,24 @@
                 scope.$watch(refreshDisplay);
 
                 function refreshDisplay() {
+                    // Use jQuery instead of jQLite
+                    element = $(element[0]);
+
+                    // Set the icon content based on the current patient profile
                     const activeProfile = ProfileSelector.getActiveProfile();
                     scope.patientInitials = activeProfile ? $filter('profileInitials')(activeProfile) : '';
-                    scope.profileColor = {'background-color': activeProfile?.color || 'grey'};
+
+                    // If synced, the profile icon only takes on its colour when it's on a selected ons tab
+                    if (scope.syncWithOnsToolbar) {
+                        // In case of issues, fall back on the default tab color
+                        const selectedColor = activeProfile?.color || '#1284ff';
+                        const isOnActiveTab = element.parents('ons-tab.active').length > 0;
+                        scope.profileColor = {
+                            'background-color': isOnActiveTab ? selectedColor : '#666',
+                        };
+                    }
+                    // Otherwise, the profile icon is always in color (or can fall back on grey in case of errors)
+                    else scope.profileColor = {'background-color': activeProfile?.color || 'grey'};
                 }
             }
         };
