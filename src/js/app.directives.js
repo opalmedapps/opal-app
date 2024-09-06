05aa25ffeaa72232827a03a2172c049c437e6d44
Anton Gladyr <anton.gladyr+opal@gmail.com>
2024-09-06 19:39:13 +0000

    refactor: remove highcharts dependency
    See merge request opalmedapps/opal-app#1238

--- src/js/app.directives.js
+++ src/js/app.directives.js
@@ -2,7 +2,6 @@ import "./directives/loading-spinning-button.directive";
 import "./directives/watchHeightOfElementDirective.js";
 import "./directives/changeHeightDiv.js";
 import "./directives/fileReaderDirective.js";
-import "./directives/lineChartDirective.js";
 import "./directives/phoneInputDirective.js";
 import "./directives/linePlusBarChartDirective_bak.js";
 import "./directives/rateEducationalMaterialDirective.js";
