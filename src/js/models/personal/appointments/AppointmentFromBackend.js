df9126f1be6a9102da0f538a0bd8c3b35712dcde
Stacey Beard <staceybeard10@gmail.com>
2024-09-18 11:36:10 -0400

    fix: fix appointment from backend loading issues

--- src/js/models/personal/appointments/AppointmentFromBackend.js
+++ src/js/models/personal/appointments/AppointmentFromBackend.js
@@ -27,26 +27,26 @@ export class AppointmentFromBackend {
         checkinStatus = checkinpossible == 0 && checkinStatus ? 'warning' : checkinStatus;
 
         this.AppointmentSerNum = appointmentsernum;
-        this.AppointmentType_EN = alias.aliasname_en;
-        this.AppointmentType_FR = alias.aliasname_fr;
+        this.AppointmentType_EN = alias?.aliasname_en;
+        this.AppointmentType_FR = alias?.aliasname_fr;
         this.Checkin = checkin;
         this.CheckinInstruction_EN = checkininstruction_en;
         this.CheckinInstruction_FR = checkininstruction_fr;
         this.CheckinPossible = checkinpossible;
         this.CheckInStatus = checkinStatus;
-        this.LastUpdated = $filter('formatDate')(lastupdated);
+        this.LastUpdated = Date.parse(lastupdated);
         this.MapDescription_EN = hospitalmap?.mapdescription_en;
         this.MapDescription_FR = hospitalmap?.mapdescription_fr;
         this.MapName_EN = hospitalmap?.mapname_en;
         this.MapName_FR = hospitalmap?.mapname_fr;
         this.MapUrl_EN = hospitalmap?.mapurl_en;
         this.MapUrl_FR = hospitalmap?.mapurl_fr;
-        this.PatientSerNum = patient.patientsernum;
-        this.ResourceDescription = language === 'EN' ? alias.aliasname_en : alias.aliasname_fr;
+        this.PatientSerNum = patient?.patientsernum;
+        this.ResourceDescription = language === 'EN' ? alias?.aliasname_en : alias?.aliasname_fr;
         this.RoomLocation_EN = roomlocation_en;
         this.RoomLocation_FR = roomlocation_fr;
-        this.ScheduledStartTime = $filter('formatDate')(scheduledstarttime);
-        this.ScheduledEndTime = $filter('formatDate')(scheduledendtime);
+        this.ScheduledStartTime = Date.parse(scheduledstarttime);
+        this.ScheduledEndTime = Date.parse(scheduledendtime);
         this.State = state;
     }
 }
