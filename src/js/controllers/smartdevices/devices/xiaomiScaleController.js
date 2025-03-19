4566e3e556b89477e3815f2a4ef0a19d0aa52524
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2025-03-19 19:01:28 -0400

    feat: BLE communication with the Xiaomi Mi Scale
    Closes OP-24
    
    See merge request opalmedapps/qplus!1233

--- src/js/controllers/smartdevices/devices/xiaomiScaleController.js
+++ src/js/controllers/smartdevices/devices/xiaomiScaleController.js
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (C) 2023 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
+// SPDX-FileCopyrightText: Copyright (C) 2024 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -7,22 +7,19 @@
 
     angular
         .module('OpalApp')
-        .controller('EtekcityScaleController', EtekcityScaleController);
+        .controller('XiaomiScaleController', XiaomiScaleController);
 
-        EtekcityScaleController.$inject = [
+    XiaomiScaleController.$inject = [
         '$scope', '$filter', '$timeout', 'Navigator', 'RequestToServer', 'Params', 'ProfileSelector'
     ];
 
     /* @ngInject */
-    function EtekcityScaleController($scope, $filter, $timeout, Navigator, RequestToServer, Params, ProfileSelector)
+    function XiaomiScaleController($scope, $filter, $timeout, Navigator, RequestToServer, Params, ProfileSelector)
     {
         // UUIDs for smart scale
-        const SERVICE_UUID = 'FFE0';
-        const BATTERY_SERVICE_UUID = '180F';
-        const BATTERY_CHARACTERISTIC_UUID = '2A19';
-        const SCALE_SERVICE_UUID = 'FFF0';
-        const NOTIFICATION_CHARACTERISTIC_UUID = 'FFF1';
-        const WRITE_CHARACTERISTIC_UUID = 'FFF2';
+        const SERVICE_UUID = '181D';
+        const SCALE_SERVICE_UUID = '181D';
+        const NOTIFICATION_CHARACTERISTIC_UUID = '2A9D';
 
         // Error messages
         const ERROR_BACKEND = $filter('translate')('SMARTDEVICES_ERROR_BACKEND');
@@ -33,19 +30,15 @@
         const SAMPLE_TYPE_WEIGHT = 'BM';
         // the patient
         const SAMPLE_SOURCE = 'P';
-        const UNIT_KG = 1;
 
         let vm = this;
 
         vm.scanning = false;
         vm.weight = null;
-        // vm.weight = 13.45;
         vm.dataSubmitted = false;
         vm.debug = false;
         vm.selectedDevice = null;
-        // vm.selectedDevice = {name: 'QN-Scale', id: '1234-5678-aaaa-bbbb', battery: 91};
         vm.errorMessage = null;
-        // vm.errorMessage = `${ERROR_BACKEND}: Patient not found`;
         
         let devices = new Map();
         vm.messages = [];
@@ -68,7 +61,7 @@
                 value: vm.weight,
                 type: SAMPLE_TYPE_WEIGHT,
                 start_date: new Date().toISOString(),
-                source: 'P',
+                source: SAMPLE_SOURCE,
                 device: vm.selectedDevice.name,
             };
 
@@ -168,11 +161,6 @@
 
         async function onConnected(device, result) {
             addMessage(`Connected to device`);
-
-            let batteryBuffer = await ble.withPromises.read(result.id, BATTERY_SERVICE_UUID, BATTERY_CHARACTERISTIC_UUID);
-            let bytes = new Uint8Array(batteryBuffer);
-            device.battery = bytes;
-            addMessage(`Battery level: ${bytes}%`);   
             
             addDebugMessage('Subscribing to receive notifications');
             await ble.withPromises.startNotification(result.id, SCALE_SERVICE_UUID, NOTIFICATION_CHARACTERISTIC_UUID, (data) => {
@@ -190,61 +178,38 @@
 
                 unsubscribe(result.id);
                 disconnect(device);
-            }, 5000);
+            }, 7000);
         }
 
         async function onSubscribeSuccess(device_id, result) {
             let value = new Uint8Array(result);
 
-            let packetType = value[0];
             addDebugMessage(`Received raw message: ${toHexString(value)}`);
-
-            if (packetType === 0x12) {
-                addDebugMessage('Device says hello');
-                // send configure response with unit = kg
-                let response = new Uint8Array([0x13, 9, 21, UNIT_KG, 16, 170, 22, 0, 2]);
-                addDebugMessage(`Sending response: ${toHexString(response)}`);
-                await ble.withPromises.write(device_id, SCALE_SERVICE_UUID, WRITE_CHARACTERISTIC_UUID, response.buffer);
-            } else if (packetType === 0x14) {
-                addDebugMessage('Device responded with unknown packet type')
+            
+            // Xiaomi Mi Scale info: https://github.com/oliexdev/openScale/wiki/Xiaomi-Bluetooth-Mi-Scale
+            if (value[0] === 0x22) {
+                addDebugMessage('Received final measurement');
                 
-                let response = mergeArraysWithChecksum([0x20, 8, 0x15], dateToBytes());
-                addDebugMessage(`Sending response: ${toHexString(response)}`);
+                // the weight is at index 1 and 2 in little endian format
+                let weightArray = value.slice(1, 3);
+                weightArray.reverse();
 
-                await ble.withPromises.write(device_id, SCALE_SERVICE_UUID, WRITE_CHARACTERISTIC_UUID, response.buffer);
-            } else if (packetType == 0x21) {
-                // no response necessary it seems, the device sends the next package (measurement) anyway
-                addDebugMessage('Device responded with second unknown packet type');
-            } else if (packetType == 0x10) {
-                addDebugMessage('Device responded with measurement packet');
+                // convert to hex string
+                // see: https://github.com/LinusU/array-buffer-to-hex/blob/master/index.js
+                let weightHexString = '';
 
-                if (value[5] === 1) {
-                    addDebugMessage('Received final measurement');
-                    
-                    // the weight is at index 3 and 4
-                    let weightArray = value.slice(3, 5);
-                    // convert to hex string
-                    // see: https://github.com/LinusU/array-buffer-to-hex/blob/master/index.js
-                    let weightHexString = '';
+                weightArray.forEach(element => {
+                    weightHexString += element.toString(16);
+                });
 
-                    weightArray.forEach(element => {
-                        weightHexString += element.toString(16);
-                    });
+                // convert to hex to int and convert to kg
+                let weight = parseInt(weightHexString, 16) / 200;
 
-                    // convert to hex to int and convert to kg
-                    let weight = parseInt(weightHexString, 16) / 100;
+                addMessage(`Weight: ${weight} kg`);
+                vm.weight = weight;
 
-                    addMessage(`Weight: ${weight} kg`);
-                    vm.weight = weight;
-
-                    let response = new Uint8Array([0x1f, 0x5, 0x15, 0x10, 0x49]);
-
-                    await ble.withPromises.write(device_id, SCALE_SERVICE_UUID, WRITE_CHARACTERISTIC_UUID, response.buffer);
-                    addDebugMessage('Told the device to stop spamming me');
-
-                    unsubscribe(device.id);
-                    disconnect(device);
-                }
+                unsubscribe(device.id);
+                disconnect(device);
             }
         }
 
@@ -275,33 +240,6 @@
             });
 
             return hexString;
-        }
-
-        function dateToBytes() {
-            // send response with timestamp with current time instead of hard-coded timestamp
-            // in seconds
-            const struct = require('python-struct');
-
-            const Y2K_START = 946684800;
-            // time is returned in milliseconds
-            let timestamp = (Date.now() / 1000) - Y2K_START;
-
-            // Python equivalent to: struct.pack("<I", int(timestamp))
-            // see: https://github.com/banksy-git/etekcity_scale/blob/master/etekcity_scale/packet.py#L50
-            return struct.pack('<I', parseInt(timestamp));
-        }
-
-        function mergeArraysWithChecksum(array1, array2) {
-            // sum of individual array lengths plus an element for the checksum
-            length = array1.length + array2.length;
-            let mergedArray = new Uint8Array(length + 1);
-            mergedArray.set(array1);
-            mergedArray.set(array2, array1.length);
-
-            let checksum = mergedArray.reduce((partialSum, value) => partialSum + value, 0) & 0xFF;
-            mergedArray.set([checksum], length);
-
-            return mergedArray;
         }
     }
 })();
