9db162d28c69cc229fe33be876bab9ad06ac7db8
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 17:48:02 +0000

    Adjustements following code review
    (cherry picked from commit 3f8e07186f11802cd67871035ebef21ebb0ee535)
    
    Co-authored-by: david.gagne <david.gagne@muhc.mcgill.ca>

--- src/js/services/patientService.js
+++ src/js/services/patientService.js
@@ -11,25 +11,23 @@ angular
     .service('Patient',['$injector','UserAuthorizationInfo','UserPreferences',
     function($injector, UserAuthorizationInfo, UserPreferences) {
 
-        var ProfileImage='';
-        var FirstName='';
-        var LastName='';
-        var accessLevel='';
-        var TelNum='';
-        var Email='';
-        var PatientSerNum = '';
-        var SelectedProfile = {};
+        let ProfileImage = '';
+        let FirstName = '';
+        let LastName = '';
+        let AccessLevel = '';
+        let Email = '';
+        let PatientSerNum = '';
+        let SelectedProfile = {};
 
         return {
-            setSelectedProfile: setSelectedProfile,
+            setSelectedProfile: profile => SelectedProfile = profile,
             initPatient: initPatient,
-            getFirstName: getFirstName,
-            getLastName: getLastName,
-            getTelNum: getTelNum,
-            getEmail: getEmail,
-            getPatientSerNum: getPatientSerNum,
-            getProfileImage: getProfileImage,
-            getAccessLevel: getAccessLevel,
+            getFirstName: () => FirstName,
+            getLastName: () => FirstName,
+            getEmail: () => Email,
+            getPatientSerNum: () => SelectedProfile?.patient_legacy_id || PatientSerNum,
+            getProfileImage: () => ProfileImage,
+            getAccessLevel: () => AccessLevel,
             clearPatient: clearPatient,
         };
 
@@ -42,12 +40,11 @@ angular
          **/
         function setPatient(patientFields) {
             if (!patientFields) throw new Error("Failed to set empty patientFields");
-            var font = window.localStorage.getItem(UserAuthorizationInfo.getUsername() + 'fontSize');
+            let font = window.localStorage.getItem(UserAuthorizationInfo.getUsername() + 'fontSize');
             UserPreferences.setFontSize(font||'large');
             FirstName = patientFields.FirstName;
             LastName = patientFields.LastName;
-            accessLevel = patientFields.AccessLevel;
-            TelNum = patientFields.TelNum;
+            AccessLevel = patientFields.AccessLevel;
             Email = patientFields.Email;
             PatientSerNum = patientFields.PatientSerNum;
             ProfileImage = (patientFields.ProfileImage && patientFields.ProfileImage !== '')
@@ -58,14 +55,6 @@ angular
         }
 
         /**
-         * @desc Set the informations for the current patient profile selected
-         * @param {object} profile  Single profile from the list of patient return by the new backend 
-         */
-        function setSelectedProfile(profile) {
-            return SelectedProfile = profile;
-        };
-
-        /**
          * @desc Requests the Patient entry for the current user and saves it in this service.
          * @returns {Promise<Object>} Resolves with the processed Patient information once downloaded and saved.
          */
@@ -74,87 +63,7 @@ angular
             let result = await RequestToServer.sendRequestWithResponse('UserPatient');
             if (!result.Data || result.Data === "empty") throw new Error("Failed to download the user's patient information; no data was returned");
             return setPatient(result.Data);
-        };
-
-        /**
-         *@ngdoc method
-         *@name getFirstName
-         *@methodOf MUHCApp.service:Patient
-         *@returns {String} Returns first name
-         **/
-        function getFirstName() {
-            return FirstName;
-        };
-
-        /**
-         *@ngdoc method
-         *@name getLastName
-         *@methodOf MUHCApp.service:Patient
-         *@returns {String} Returns last name
-         **/
-        function getLastName() {
-            return LastName;
-        };
-
-        /**
-         *@ngdoc method
-         *@name getTelNum
-         *@methodOf MUHCApp.service:Patient
-         *@returns {String} Returns Tel. Number
-         **/
-        function getTelNum() {
-            return TelNum;
-        };
-
-        /**
-         *@ngdoc method
-         *@name getTelNum
-         *@methodOf MUHCApp.service:Patient
-         *@returns {String} Returns Tel. Number
-         **/
-        function getTelNum() {
-            return TelNum;
-        };
-
-        /**
-         *@ngdoc method
-         *@name getEmail
-         *@methodOf MUHCApp.service:Patient
-         *@returns {String} Returns Email
-         **/
-        function getEmail() {
-            return Email;
-        };
-    
-        /**
-         *@ngdoc method
-         *@name getPatientSerNum
-         *@methodOf MUHCApp.service:Patient
-         *@returns {String} Returns PatientSerNum
-         **/
-        function getPatientSerNum() {
-            return SelectedProfile?.patient_legacy_id || PatientSerNum;
-        };
-
-        /**
-         *@ngdoc method
-         *@name getProfileImage
-         *@methodOf MUHCApp.service:Patient
-         *@returns {String} Returns patient image
-        **/
-        function getProfileImage() {
-            return ProfileImage;
-        };
-
-        /**
-         *@ngdoc method
-         *@name getAccessLevel
-         *@methodOf MUHCApp.service:Patient
-         *@returns {String} Returns patient access level
-         **/
-        function getAccessLevel() {
-            return accessLevel;
-        };
+        }
 
         /**
          *@ngdoc method
@@ -163,13 +72,13 @@ angular
          *@description Cleans the patient service
          **/
         function clearPatient() {
-            ProfileImage='';
-            FirstName='';
-            LastName='';
-            TelNum='';
-            Email='';
-            PatientSerNum='';
-            SelectedProfile={};
-        };    
+            ProfileImage = '';
+            FirstName = '';
+            LastName = '';
+            Email = '';
+            PatientSerNum = '';
+            AccessLevel = '';
+            SelectedProfile = {};
+        }
     }
 ]);
