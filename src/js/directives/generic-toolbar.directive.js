a5035a285701af2c18493107240ca4b1795c224a
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 17:43:04 +0000

    Merge branch 'staging' into OPAL-1015.DG.new-message-visual-notification
    (cherry picked from commit c55516c50c4282e7a30d61c4d72091424c2e98e6)
    
    Co-authored-by: David Gagne <david.gagne@muhc.mcgill.ca>

---
+++ src/js/directives/generic-toolbar.directive.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+
+    angular
+        .module("MUHCApp")
+        .directive("genericToolbar", GenericToolbar);
+
+    /**
+     * @name TopToolbar
+     * @author David Gagne
+     * @date 2021-12-10
+     * @desc Directive for the top tool bar with title and back bouton.
+     */
+    function GenericToolbar()
+    {
+        return {
+            restrict: 'E',
+            scope: {
+                title: '@',
+                backButton: '=?',
+                styleClass: '=',
+            },
+            transclude: {
+                'leftContentSlot': '?leftContent',
+                'rightContentSlot': '?rightContent'
+            },
+            template: `
+                <ons-toolbar fixed-style>
+                    <div class="left" ng-transclude="leftContentSlot"><ons-back-button ng-hide="backButton === false">{{"BACK"|translate}}</ons-back-button></div>
+                    <div class="center overflow-text-ellipsis" ng-class="styleClass">{{title}}</div>
+                    <div class="right" ng-transclude="rightContentSlot"></div>
+                </ons-toolbar>
+            `
+        };
+    }
+})();
