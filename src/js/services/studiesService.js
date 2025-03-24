7fdada6a9cd8c87aa78ab420e69806f441cfe7ca
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 17:48:53 +0000

    feat: "Research Menu" studies
    * chore: add todo for the studies page
    
    * refactor: add TODO comment for fetching badges
    
    * fix: update translation strings
    
    * fix: address issues related to the info tab
    
    * fix: update getStudies() method
    
    * fix: update service and controller for fetching studies list
    
    * fix: add goToClinicalQuestionnaire function
    
    * refactor: use generic-toolbar directive
    
    * refactor: use generic-toolbar directive
    
    * refactor: update updateConsentStatus method
    
    * refactor: update getStudies function
    
    * refactor: use async/await for loading studies
    
    * refactor: cosmetic code fixes
    
    * feat: add research menu tab
    
    * feat: "Research Menu" studies
    
    See merge request opalmedapps/qplus!939
    
    
    (cherry picked from commit c56c3363e41295981a0a09400ef71498d96f8b20)
    
    a59e40e1 feat: "Research Menu" studies
    5ad1540e feat: add research menu tab
    1e862256 refactor: cosmetic code fixes
    7cec584e refactor: use async/await for loading studies
    8e5a6f21 refactor: update getStudies function
    f6e4d751 refactor: update updateConsentStatus method
    c54ac3f8 refactor: use generic-toolbar directive
    862747d1 refactor: use generic-toolbar directive
    93777a40 fix: add goToClinicalQuestionnaire function
    bd465e88 fix: update service and controller for fetching studies list
    3bd3de50 fix: update getStudies() method
    f112939c fix: address issues related to the info tab
    31e67992 fix: update translation strings
    ff83efa2 refactor: add TODO comment for fetching badges
    98b2570b Merge branch 'staging' into QSCCD-653.AG.research-menu-studies
    4e09acae chore: add todo for the studies page
    
    Co-authored-by: Gladyr Anton <anton.gladyr@muhc.mcgill.ca>

---
+++ src/js/services/studiesService.js
@@ -0,0 +1,119 @@
+/*
+ * Filename     :   studiesService.js
+ * Description  :   Service that store and manages the studies information.
+ * Created by   :   Kayla O'Sullivan-Steben
+ * Date         :   March 2021
+ */
+(function () {
+    angular
+        .module('MUHCApp')
+        .service('Studies', StudiesService);
+
+    StudiesService.$inject = ['RequestToServer', '$filter', '$q'];
+
+    function StudiesService(RequestToServer, $filter, $q) {
+
+        var studies = [];
+
+        return {
+            getNumberUnreadStudies: getNumberUnreadStudies,
+            getStudies: getStudies,
+            getStudiesList: getStudiesList,
+            readStudy: readStudy,
+            updateConsentStatus: updateConsentStatus
+        };
+
+
+        /**
+         * @name getStudies
+         * @desc Requests the list of all studies from the listener and stores it in the studies array.
+         * @return {promise} 
+         */
+        async function getStudies() {
+            try {
+                let response = await RequestToServer.sendRequestWithResponse('Studies');
+
+                // check if response contains studies
+                if (!response?.Data?.studies) return [];
+
+                studies = response.Data.studies;
+
+                studies.forEach(function (study) {
+                    study.creationDate = $filter('formatDate')(study.creationDate)
+                    if (study?.startDate) {
+                        study.startDate = $filter('formatDate')(study.startDate)
+                    }
+                    if (study?.endDate) {
+                        study.endDate = $filter('formatDate')(study.endDate)
+                    }
+                });
+
+                return studies;
+            } catch (error) {
+                console.error('Error in getStudies: ', error);
+                return [];
+            }
+
+        }
+
+
+        /**
+         * @name updateConsentStatus
+         * @desc Updates the consent status for the study corresponding to the given consent form Id.
+         * @param {int} questionnaireId The ID of the consent form (questionnaire Id in QuestionnaireDB).
+         * @param {string} status The consent status submitted, either "opalConsented" or "declined".
+         * @return {promise} 
+         */
+        async function updateConsentStatus(questionnaireId, status) {
+            let params = {
+                questionnaire_id: questionnaireId,
+                status: status
+            };
+
+            try {
+                let response = await RequestToServer.sendRequestWithResponse('StudyUpdateStatus', params);
+
+                return { Success: true, Location: 'Server' };
+            } catch (error) {
+                throw { Success: false, Location: '', Error: error };
+            }
+        }
+
+        /**
+        * @name getStudiesList
+        * @desc Returns the current list of studies.
+        * @return {Array} Array of studies.
+        */
+        function getStudiesList() {
+            return studies;
+        }
+
+        /**
+         *@name readStudy
+         *@desc Updates the ReadStatus of a study in studies array and sends request to backend
+         *@param {int} patientStudyId ID of the study in the patientStudy table to be read
+         **/
+        function readStudy(patientStudyId) {
+            for (var i = 0; i < studies.length; i++) {
+                if (studies[i].patientStudyId == patientStudyId) {
+                    studies[i].ReadStatus = '1';
+                    RequestToServer.sendRequest('Read', { 'Id': patientStudyId, 'Field': 'patientStudy' });
+                }
+            }
+        }
+
+        /**
+         * @name getNumberUnreadStudies
+         * @description Iterates through the studies array and returns the number of unread studies.
+         * @returns {Number} Returns number of unread studies
+         **/
+        function getNumberUnreadStudies() {
+            let number = 0;
+            for (let i = 0; i < studies.length; i++) {
+                if (studies[i].ReadStatus === '0') number++;
+            }
+
+            return number;
+        }
+    }
+})();
