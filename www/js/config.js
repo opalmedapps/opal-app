246bcf79b8d19f78f2faa4b3bf551a453946092b
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 17:38:59 +0000

    Merge branch 'opal_dev' into issue252
    (cherry picked from commit dd2c7148579dbeaf589bf7d8f646639981e47eb5)
    
    Co-authored-by: JamesBrace <bracejames95@gmail.com>

--- www/js/config.js
+++ www/js/config.js
@@ -5,13 +5,11 @@ fireConfig.$inject = [];
 /* @ngInject */
 function fireConfig () {
     var config = {
-
         apiKey: "AIzaSyAMIDdcQR8EiY9gjj4cgxp6Vu3xwa78Ww8",
         authDomain: "opal-dev.firebaseapp.com",
         databaseURL: "https://opal-dev.firebaseio.com",
         storageBucket: "opal-dev.appspot.com",
         messagingSenderId: "652464215237"
-
     };
 
     if (!firebase.apps.length) {
