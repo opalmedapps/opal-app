e19f82c704b8cf930dea3f39f2a57f7430090613
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 18:28:29 +0000

    chore: uninstall npm version of the library, which is unused
    (cherry picked from commit 008bf994de951fc1d921fefbd7efbb09a0bdb7a1)
    
    Co-authored-by: Stacey Beard <staceybeard10@gmail.com>

--- src/js/app.js
+++ src/js/app.js
@@ -74,7 +74,6 @@
 import "jquery";
 import angular from "angular";
 import "angular-animate";
-import "angular-ui-bootstrap";
 import "../lib/ui-bootstrap-tpls-2.5.6-opal.js";
 import "angular-translate";
 import "angular-dynamic-locale";
