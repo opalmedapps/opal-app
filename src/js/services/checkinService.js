9f73dddc4a9a4a44fb6b94b44184586a97f2833f
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 17:53:56 +0000

    fix deps remove testing code
    (cherry picked from commit a7f369392d67fbe3142776bbdf114c674cacd22d)
    
    Co-authored-by: LiminLiu <Limin.Liu@muhc.mcgill.ca>

--- src/js/services/checkinService.js
+++ src/js/services/checkinService.js
@@ -263,7 +263,6 @@
          *                             geolocated.
          */
         async function isWithinCheckinRange() {
-            return true;
             // Get the list of sites and their coordinates from the backend
             const response = await Hospital.requestSiteInfo(UserHospitalPreferences.getHospital());
             if (!response?.count || response?.count === '0') throw new Error("No sites are defined for this institution");
