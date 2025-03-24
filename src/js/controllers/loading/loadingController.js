b7d5b7dc4d65fc7b81fb2e9813ccf64af655e1c3
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 18:21:25 +0000

    extra line
    (cherry picked from commit e4d16492e260aeb4fd842bcea5dd8f40b3b578c5)
    
    Co-authored-by: Napparific <kelly.agnew@mail.mcgill.ca>

--- src/js/controllers/loading/loadingController.js
+++ src/js/controllers/loading/loadingController.js
@@ -31,8 +31,7 @@
                 await UpdateUI.init();
                 await RequestToServer.sendRequestWithResponse('AccountChange', {NewValue: UserPreferences.getLanguage(), FieldToChange: 'Language'});
                 await MetaData.init();
-                await ProfileSelector.init();
-                      
+                await ProfileSelector.init();     
                 // TODO: Currently, the app isn't able to handle a state in which there are no confirmed profiles. In this case, cancel login.
                 if (ProfileSelector.getConfirmedProfiles().length === 0) {
                     console.error('Cannot log in when there are no confirmed profiles.');
