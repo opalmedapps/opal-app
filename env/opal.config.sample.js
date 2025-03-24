48cc9f649daeacb7a5dcab592299b07e7583f71e
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 18:29:09 +0000

    fix: clean up for MR and finish documentation changes
    (cherry picked from commit 900e7a3b527b9ed61a0f04b5b0f83fed2514d642)
    
    Co-authored-by: Stacey Beard <staceybeard10@gmail.com>

--- env/opal.config.sample.js
+++ env/opal.config.sample.js
@@ -1,9 +1,9 @@
 // The following example is well-suited for a development environment. Check each variable carefully before using in production.
 const config = {
     // The name of your app
-    name: "Opal Sample",
+    name: "Opal Local",
     // The name of your environment. Should match the parent folder name.
-    env: "sample",
+    env: "local",
     // Environment-specific settings
     settings: {
         // Boolean: whether to kick out a user when another person logs into the same user account on another device.
@@ -12,27 +12,27 @@ const config = {
         messageOfTheDayKey: "",
         // Boolean: whether to show the app's version and build number on the front page.
         showVersionOnInit: true,
-        // Boolean: whether to use real (production-ready) institution names and acronyms (such as "MUHC"). If false, generic names are used.
+        // Boolean: whether to use real (production-ready) institution names and acronyms (such as "RI-MUHC"). If false, generic names are used.
         useRealInstitutionNames: false,
         // Boolean: whether to use a sourcemap when building the web code. Should be false in production.
         useSourceMap: true,
-        // Boolean: allow the user to take the screenshots or not.
+        // Boolean: whether screenshots can be taken in the app. Should be false in production.
         screenshotsAllowed: true,
         // String: the mode to use when building with Webpack: [production, development].
         webpackMode: "development",
     },
-    // Placeholder values that are replaced in config.xml when building the app
+    // Placeholders that are inserted into config.xml when building the app
     configXml: {
-        // String: app ID; must match the value used in Firebase App Distribution or in the app stores.
-        "APP_ID": "com.hig.opalsample",
+        // String: app ID (usually starts with `com.`); must match the value used in Firebase App Distribution or in the app stores.
+        "APP_ID": "",
         // String: the display name of the app when installed on a device.
-        "APP_NAME": "Opal Sample",
-        // Integer: the build number shown in brackets after the app's version number. Prod requires a higher value for store uploads.
+        "APP_NAME": "Opal Local",
+        // Integer: the build number shown in brackets after the app's version number. Must always increase for consecutive store uploads.
         "BUILD_NUMBER": 1,
-        // Boolean: whether the Android app is debuggable (android:debuggable).
+        // Boolean: whether the Android app is debuggable (android:debuggable). Should be false in production.
         "ANDROID_DEBUGGABLE": true,
     },
-    // Block of Firebase web configurations, with contents pasted from the Firebase Console
+    // Firebase web configurations, with contents pasted from the Firebase Console
     firebase: {
         apiKey: "",
         authDomain: "",
