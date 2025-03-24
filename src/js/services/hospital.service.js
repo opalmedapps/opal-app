bbddff50a95118e4297978e594df4808f76649d6
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 18:05:18 +0000

    update
    (cherry picked from commit 70b33e183215db2f890979f126eccd3ae53d07d4)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- src/js/services/hospital.service.js
+++ src/js/services/hospital.service.js
@@ -19,10 +19,10 @@
 
     /* @ngInject */
     function Hospital(RequestToServer, Params) {
-        const endpoint = Params.API.ROUTES.HOSPITAL_SETTINGS.SITES;
 
         let requestHospitalService = {
             requestSiteInfo: requestSiteInfo,
+            requestInstitutionInfo: requestInstitutionInfo,
         };
 
         return requestHospitalService;
@@ -34,8 +34,17 @@
          * @desc this function gets site info for the sites linked to a particular hospital
          * @returns {Promise} resolves to the sites' data if success
          */
-        async function requestSiteInfo(){
-            const result = await RequestToServer.apiRequest(endpoint);
+        async function requestSiteInfo() {
+            const result = await RequestToServer.apiRequest(Params.API.ROUTES.HOSPITAL_SETTINGS.SITES);
+            return result.data;
+        }
+
+        /**
+         * @desc this function gets institution info for the institution the user is connected to
+         * @returns {Promise} resolves to the institution data if success
+         */
+        async function requestInstitutionInfo() {
+            const result = await RequestToServer.apiRequest(Params.API.ROUTES.HOSPITAL_SETTINGS.INSTITUTION);
             return result.data;
         }
     }
