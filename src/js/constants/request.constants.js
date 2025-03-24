0dd5a9ef1c5aa02f15383677f7fce593c2d2e368
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 17:53:21 +0000

    add Studies to the patient target requests   -  deny users permission to access Study data when they don’t have a relationship with the patient.
    (cherry picked from commit 930ff9cfdb68e923c88398ad0daba0410ddeb0e9)
    
    Co-authored-by: Shifeng Chen <shifeng.chen@muhc.mcgill.ca>

--- src/js/constants/request.constants.js
+++ src/js/constants/request.constants.js
@@ -35,6 +35,7 @@
                 'QuestionnaireSaveAnswer',
                 'QuestionnaireUpdateStatus',
                 'Refresh',
+                'Studies',
             ],
         });
 })();
