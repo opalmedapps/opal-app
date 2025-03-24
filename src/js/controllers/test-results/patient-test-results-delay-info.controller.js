d9d7093f411134aaa1a241d03b5c061272b85bc0
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 18:05:20 +0000

    update
    (cherry picked from commit 5db0d28d617dca6ea1ea73f57b693c634947e70f)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

---
+++ src/js/controllers/test-results/patient-test-results-delay-info.controller.js
@@ -0,0 +1,47 @@
+/**
+ * @file Controller for the delay info page
+ */
+(function () {
+	'use strict';
+
+	angular
+		.module('MUHCApp')
+		.controller('PatientTestResultsDelayInfoController', PatientTestResultsDelayInfoController);
+
+		PatientTestResultsDelayInfoController.$inject = ['Hospital', 'Params', '$timeout'];
+
+	function PatientTestResultsDelayInfoController(Hospital, Params, $timeout) {
+
+		const vm = this;
+		vm.loading = true;
+
+		activate();
+
+		//////////////////////////////////////////
+
+		async function activate() {
+			try {
+				let institution = await Hospital.requestInstitutionInfo();
+				console.log(institution);
+
+				$timeout(() => {
+					vm.default_interpretable = institution.interpretable_lab_result_delay;
+					vm.default_non_interpretable = institution.non_interpretable_lab_result_delay;
+                    vm.loading = false;
+				});
+			} catch (error) {
+                $timeout(() => {
+                    // TODO: Error handling improvements: https://o-hig.atlassian.net/browse/QSCCD-463
+                    console.error(error);
+
+                    vm.loading = false;
+
+                    vm.alert = {
+                        type: Params.alertTypeDanger,
+                        content: "PAGE_ACCESS_ERROR"
+                    };
+                })
+			}
+		}
+	}
+})();
