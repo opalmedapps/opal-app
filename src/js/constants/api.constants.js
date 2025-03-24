cf2aac2215c536b5f721671eeb0c1809ff9d1bff
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 17:47:02 +0000

    QSCCD-461: General view data request to new API
    (cherry picked from commit fbad239e25a688e5e4e37ab47161eda0ed5949f1)
    
    Co-authored-by: Shifeng Chen <shifeng.chen@muhc.mcgill.ca>

--- src/js/constants/api.constants.js
+++ src/js/constants/api.constants.js
@@ -25,6 +25,10 @@
                 CHART: {
                     method: 'get',
                     url: '/api/app/chart',
+                },
+                GENERAL: {
+                    method: 'get',
+                    url: '/api/app/general'
                 }
             }
         });
