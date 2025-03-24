8b95dc8152b77a5ebf97c461096aaf62905664bd
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-24 17:45:03 +0000

    Clean up the labs controller and service based on the new functionality
    (cherry picked from commit fd231d3d472e4800bf31ed4b5965a211bf68d610)
    
    Co-authored-by: Stacey Beard <staceybeard10@gmail.com>

--- src/js/controllers/test-results/patient-test-results.controller.js
+++ src/js/controllers/test-results/patient-test-results.controller.js
@@ -5,15 +5,12 @@
 		.module('MUHCApp')
 		.controller('PatientTestResultsController', PatientTestResultsController);
 
-	PatientTestResultsController.$inject = ['PatientTestResults', 'UserPreferences', 'Toast',
-		'NavigatorParameters', '$filter', '$timeout'];
+	PatientTestResultsController.$inject = ['$filter','NavigatorParameters','PatientTestResults','UserPreferences'];
 
-	function PatientTestResultsController(PatientTestResults, UserPreferences, Toast, NavigatorParameters, $filter, $timeout) {
+	function PatientTestResultsController($filter, NavigatorParameters, PatientTestResults, UserPreferences) {
 
 		let vm = this;
 
-		// TODO move?
-		const SERVER_REFRESH_TIME = 60000;
 		/**
 		 * Array holding the patient test dates
 		 * @type {Date[]}
@@ -25,49 +22,22 @@
 		 */
 		vm.searchTestTypeText = "";
 		/**
-		 * Loading variable for the loading bar in the view
-		 * @type {boolean}
-		 */
-		vm.loading = true; // DELETE
-		/**
 		 * Whether the view should display the date list or the type list
 		 * @type {boolean}
 		 */
 		vm.showByDate = true;
-		/**
-		 * Can be used to toggle visibility of the content section without using the loading wheel.
-		 * Used to force a refresh of the content.
-		 * @type {boolean}
-		 */
-		vm.showContent = true;
-		/**
-		 * Tracks whether the user has just refreshed the labs from the Type tab.
-		 * If this is the case, the Date tab's display must be forced to update when next visiting it, or it stays blank.
-		 * @type {boolean}
-		 */
-		let justRefreshedByType = false;
-		/**
-		 * @description Tracks whether the user is currently being shown a warning that the labs can't be refreshed this soon.
-		 *              Prevents the user from enqueuing a lot of duplicate messages in the Toast service.
-		 * @type {boolean}
-		 */
-		let refreshWarningIsShowing = false;
 
 		let language;
 		let navigator;
 
-		vm.goToTestDateResults = goToTestDateResults
-		vm.goToTestTypeResults = goToTestTypeResults
-		vm.showDateHeader = showDateHeader
-		vm.displayType = displayType
-		vm.getTestClass = getTestClass
-		vm.checkForRefresh = checkForRefresh
-		vm.updateViewFromService = updateViewFromService
-		vm.shouldFetchTestResultsFromServer = shouldFetchTestResultsFromServer
-		vm.getTestResultsMetadata = getTestResultsMetadata
-		vm.updateView = updateView
-		vm.refreshDisplay = refreshDisplay
-		vm.handlerServerError = handlerServerError
+		vm.goToTestDateResults = goToTestDateResults;
+		vm.goToTestTypeResults = goToTestTypeResults;
+		vm.showDateHeader = showDateHeader;
+		vm.displayType = displayType;
+		vm.getTestClass = PatientTestResults.getTestClass;
+
+		// Used by patient-data-handler
+		vm.setTestsView = setTestsView;
 
 		activate();
 
@@ -80,35 +50,8 @@
 		function activate() {
 			language = UserPreferences.getLanguage();
 			navigator = NavigatorParameters.getNavigator();
-			// getTestResultsMetadata(this.#shouldFetchTestResultsFromServer())
-			// 	.then(this.#updateView)
-			// 	.catch(this.#handlerServerError);
 		}
 
-		/**
-		 * Refreshes the patient test result metadata only if the last updated time is larger than SERVER_REFRESH_TIME
-		 */
-		// refresh() {
-		// 	// Only allowed to refresh once every 60s.
-		// 	if (this.#shouldFetchTestResultsFromServer()) {
-		// 		this.loading = true;
-		// 		if (!this.showByDate) this.#justRefreshedByType = true;
-		// 		this.getTestResultsMetadata(true)
-		// 			.then(this.#updateView)
-		// 			.catch(this.#handlerServerError);
-		// 	}
-		// 	// Use a variable to prevent new refresh warnings from being added to the toast queue if one is already showing
-		// 	else if (!this.#refreshWarningIsShowing) {
-		// 		this.#refreshWarningIsShowing = true;
-		// 		this.#toast.showToast({
-		// 			message: this.#$filter('translate')("REFRESH_WAIT"),
-		// 			position: "bottom",
-		// 			callback: () => {
-		// 				this.#refreshWarningIsShowing = false;
-		// 			}
-		// 		});
-		// 	}
-		// }
 		/**
 		 * Function to go to the `test-results-by-datetime.html` view, passes the testDate for the view
 		 * @param {Date} testDate Test date for the subsequent view
@@ -118,6 +61,7 @@
 				'./views/personal/test-results/test-results-by-datetime.html',
 				{ testDate });
 		}
+
 		/**
 		 * Function to go to the `test-results-by-type.html`view, passes the testTypeSerNum for the view
 		 * @param {number} testTypeSerNum ExpressionSerNum for the given test type
@@ -141,6 +85,7 @@
 				&& dateOne.getDate() === dateTwo.getDate() &&
 				dateOne.getMonth() === dateTwo.getMonth());
 		}
+
 		/**
 		 * Returns the display name in the right language for a given TestType.
 		 * @param {PatientTestType} type TestType
@@ -150,96 +95,12 @@
 		}
 
 		/**
-		 * Returns the class for a given test based on its criticality (see forwarded function for details)
-		 */
-		function getTestClass(test) {
-			return PatientTestResults.getTestClass(test);
-		}
-
-		/**
-		 * @name checkForRefresh
-		 * @author Stacey Beard
-		 * @date 2021-05-19
-		 * @desc Checks whether a refresh of the display is required.
-		 *       The display must be refreshed on the Date tab after the lab data was refreshed from the Type tab
-		 *       (otherwise, the Date tab stays blank).
-		 *       However, it does not need to be refreshed after the lab data was refreshed from the Date tab (no issues).
-		 */
-		function checkForRefresh() {
-			if (vm.showByDate && justRefreshedByType) {
-				refreshDisplay();
-				justRefreshedByType = false;
-			}
-		}
-
-		/**
 		 * @description Gets the test results from the service and uses them to update the view.
+		 *              Results are sorted in reverse chronological order for dates, and alphabetical order for types.
 		 */
-		function updateViewFromService() {
-			console.log("Called updateViewFromService");
-			//this.#updateView(...this.#getTestResultsMetadata(false));
-
-			vm.testDates = PatientTestResults.getTestDates();
-			vm.testTypes = PatientTestResults.getTestTypes();
-			console.log(vm.testDates);
-			console.log(vm.testTypes);
-		}
-
-		////////////////////////////////////////
-
-		/**
-		 * Returns whether or not to fetch test results from server
-		 * @returns {boolean} whether or not to fetch test results from server
-		 */
-		function shouldFetchTestResultsFromServer() {
-			return Date.now() - PatientTestResults.getLastUpdated() > SERVER_REFRESH_TIME;
-		}
-
-		/**
-		 * Polls the {@link PatientTestResults} service for the testDates, and testTypes of the patient
-		 */
-		async function getTestResultsMetadata() {
-			return [PatientTestResults.getTestDates(), PatientTestResults.getTestTypes()];
-		}
-
-		/**
-		 * Updates the view with the test dates and test types for the given patient.
-		 * @param {[Date[], PatientTestType[]]} param0 testDates and testTypes for the view
-		 */
-		function updateView([testDates, testTypes]) {
-			$timeout(() => {
-				vm.loading = false;
-				vm.testDates = testDates;
-				vm.testTypes = testTypes;
-			});
-		}
-
-		/**
-		 * @name refreshDisplay
-		 * @author Stacey Beard
-		 * @date 2021-05-19
-		 * @desc Forces a refresh of the content displayed in the lab results view.
-		 *       This is necessary after refreshing the lab results from the Type tab (after which the Date tab
-		 *       breaks and becomes empty until this function is called).
-		 */
-		function refreshDisplay() {
-			vm.showContent = false;
-			$timeout(() => {
-				vm.showContent = true;
-			});
-		}
-
-		/**
-		 * Handles a response error from the server. Displays server error alert:
-		 * At the time, Opal does not have a logging mechanism for errors.
-		 */
-		function handlerServerError () {
-			// TODO(dherre3) Add logging of back-end error
-			ons.notification.alert({
-				//message: 'Server problem: could not fetch data, try again later',
-				message: $filter('translate')("SERVERERRORALERT"),
-				modifier: (ons.platform.isAndroid()) ? 'material' : null
-			});
+		function setTestsView() {
+			vm.testDates = PatientTestResults.getTestDates().sort((a, b) => b.getTime() - a.getTime()); // Newest first
+			vm.testTypes = $filter('orderBy')(PatientTestResults.getTestTypes(), `name_${language}`);
 		}
 	}
 })();
--- src/js/services/patient-test-results.service.js
+++ src/js/services/patient-test-results.service.js
@@ -17,53 +17,42 @@ import { PatientTestResultDetailed } from "../models/personal/test-results/Patie
 	function PatientTestResults(RequestToServer) {
 
 		/**
-		 * @type {number} Timestamp for the last update on types
+		 * Cache of test dates for the patient
+		 * @type {Date[]}
 		 */
-		let testTypesLastUpdated = 0;
-		/**
-		 * @type {number} Timestamp for the last update on dates
-		 */
-		let testDatesLastUpdated = 0;
+		let testDates = [];
 		/**
 		 * Cache of test types for the patient
 		 * @type {PatientTestType[]}
 		 */
 		let testTypes = [];
 		/**
-		 * Cache of test dates for the patient
-		 * @type {Date[]}
-		 */
-		let testDates = [];
-		/**
-		 * Cache of test results by date, it contains test results up to the {@link #lastUpdated}
+		 * Cache of test results by date.
 		 * @type {Record<string,*>} testResultsByDate contains map from string date to list of group types in that date
 		 */
 		let testResultsByDate = {};
 		/**
-		 * Cache of test results by type, it contains test results up to the {@link #lastUpdated}
+		 * Cache of test results by type.
 		 * @type {Record<string, PatientTestType>} testResultsByDate contains map from string date to list of type test results
 		 *                            in that date
 		 */
 		let testResultsByType = {};
 
 		let service = {
-			processDates: processDates,
-			processTypes: processTypes,
 			setTestDates: setTestDates,
 			setTestTypes: setTestTypes,
 			updateTestDates: updateTestDates,
 			updateTestTypes: updateTestTypes,
-			getTestTypes: getTestTypes,
-			getTestDates: getTestDates,
-			getTestResultsByType: getTestResultsByType,
-			testResultByTypeIsCached: testResultByTypeIsCached,
-			testResultByTypeDeleteCached: testResultByTypeDeleteCached,
+			getTestDates: () => testDates,
+			getTestTypes: () => testTypes,
 			getTestResultsByDate: getTestResultsByDate,
-			getLastUpdated: getLastUpdated,
-			testResultByDateIsCached: testResultByDateIsCached,
-			testResultByDateDeleteCached: testResultByDateDeleteCached,
+			getTestResultsByType: getTestResultsByType,
 			getTestClass: getTestClass,
 			clear: clear,
+
+			// Exported for testing
+			testResultByDateIsCached: testResultByDateIsCached,
+			testResultByTypeIsCached: testResultByTypeIsCached,
 		};
 
 		return service;
@@ -81,86 +70,42 @@ import { PatientTestResultDetailed } from "../models/personal/test-results/Patie
 		}
 
 		function setTestDates(newDates) {
-			console.log("Set test dates");
-			testDatesLastUpdated = Date.now();
 			testDates = processDates(newDates);
 			testResultsByDate = {};
-			console.log(testDates);
 		}
 
 		function setTestTypes(newTypes) {
-			console.log("Set test types");
-			testTypesLastUpdated = Date.now();
 			testTypes = processTypes(newTypes);
 			testResultsByType = {};
-			console.log(testTypes);
 		}
 
 		function updateTestDates(newTestDates) {
-			console.log("Update dates");
 			newTestDates = processDates(newTestDates);
 
 			// Dates are simple objects with only one relevant value; simply add the new ones that are missing from the array
-			let testDatesAsTime = testDates.map(e => e.getTime());
-			newTestDates.forEach(e => { if (!testDatesAsTime.includes(e.getTime())) testDates.push(e) });
+			let testDatesAsTime = testDates.map(testDate => testDate.getTime());
+			newTestDates.forEach(newDate => { if (!testDatesAsTime.includes(newDate.getTime())) testDates.push(newDate) });
 
-			// Delete cached data by date for *all* the updated items
+			// Delete cached data by date for all the updated items
 			newTestDates.forEach(e => testResultByDateDeleteCached(e));
 		}
 
 		function updateTestTypes(newTestTypes) {
-			console.log("Update types");
-			//newTestTypes = processTypes(newTestTypes);
+			newTestTypes = processTypes(newTestTypes);
 
-			// TODO finish writing
+			// Apply the new types over the old types (overwriting any matching types that are already present, by SerNum)
+			newTestTypes.forEach(newType => {
+				// If applicable, find and delete an element with the same SerNum as the new one
+				const newSerNum = newType.testExpressionSerNum;
+				const existingTypeIndex = testTypes.findIndex(type => type.testExpressionSerNum === newSerNum);
+				if (existingTypeIndex !== -1) testTypes.splice(existingTypeIndex, 1); // Delete it
 
-		}
+				// Add the new element
+				testTypes.push(newType);
 
-		/**
-		 * Returns list of test types for the patient
-		 * @returns {PatientTestType[]} Upon full-filling promise it returns list of test types for the patient
-		 */
-		function getTestTypes() {
-			console.log("Get test types: ", testTypes);
-			return testTypes;
-		}
-
-		/**
-		 * Returns test dates for the patient
-		 * @returns {Date[]} Promise fulfills with patient dates
-		 */
-		function getTestDates() {
-			console.log("Get test dates: ", testDates);
-			return testDates;
-		}
-
-		/**
-		 * Returns test type results for the patient
-		 * @param typeSerNum ExpressionSerNum
-		 * @returns {Promise<PatientTestType>} Returns results for the given test type
-		 */
-		async function getTestResultsByType(typeSerNum) {
-			if (testResultByTypeIsCached(typeSerNum)) {
-				return testResultsByType[typeSerNum];
-			} else {
-				let results = await RequestToServer.sendRequestWithResponse("PatientTestTypeResults",
-					{ testTypeSerNum: typeSerNum });
-				results = results.data || [];
-				testResultsByType[typeSerNum] = new PatientTestType(results);
-				return testResultsByType[typeSerNum];
-			}
-		}
-
-		function testResultByTypeIsCached(type) {
-			return testResultsByType.hasOwnProperty(type);
-		}
-
-		/**
-		 * If a cache exists for this type, deletes it.
-		 * @param type
-		 */
-		function testResultByTypeDeleteCached(type) {
-			if (testResultByTypeIsCached(type)) delete testResultsByType[type];
+				// Delete cached data by type for the new element
+				testResultByTypeDeleteCached(newType.testExpressionSerNum);
+			});
 		}
 
 		/**
@@ -182,11 +127,20 @@ import { PatientTestResultDetailed } from "../models/personal/test-results/Patie
 		}
 
 		/**
-		 * Get last updated time for the patient test metadata
-		 * @returns {number} returns time in milliseconds since the test metadata was last updated
+		 * Returns test type results for the patient
+		 * @param typeSerNum ExpressionSerNum
+		 * @returns {Promise<PatientTestType>} Returns results for the given test type
 		 */
-		function getLastUpdated() {
-			return Math.min(testDatesLastUpdated, testTypesLastUpdated);
+		async function getTestResultsByType(typeSerNum) {
+			if (testResultByTypeIsCached(typeSerNum)) {
+				return testResultsByType[typeSerNum];
+			} else {
+				let results = await RequestToServer.sendRequestWithResponse("PatientTestTypeResults",
+					{ testTypeSerNum: typeSerNum });
+				results = results.data || [];
+				testResultsByType[typeSerNum] = new PatientTestType(results);
+				return testResultsByType[typeSerNum];
+			}
 		}
 
 		/**
@@ -198,12 +152,24 @@ import { PatientTestResultDetailed } from "../models/personal/test-results/Patie
 			return testResultsByDate.hasOwnProperty(date.toString());
 		}
 
+		function testResultByTypeIsCached(typeSerNum) {
+			return testResultsByType.hasOwnProperty(typeSerNum);
+		}
+
 		/**
 		 * If a cache exists for this date, deletes it.
 		 * @param date
 		 */
 		function testResultByDateDeleteCached(date) {
 			if (testResultByDateIsCached(date)) delete testResultsByDate[date.toString()];
+		}
+
+		/**
+		 * If a cache exists for this type, deletes it.
+		 * @param typeSerNum
+		 */
+		function testResultByTypeDeleteCached(typeSerNum) {
+			if (testResultByTypeIsCached(typeSerNum)) delete testResultsByType[typeSerNum];
 		}
 
 		/**
@@ -227,13 +193,10 @@ import { PatientTestResultDetailed } from "../models/personal/test-results/Patie
 		 * Upon signing out, clear test results
 		 */
 		function clear() {
-			console.log("Tests cleared");
-			testResultsByDate = {};
-			testResultsByType = {};
 			testDates = [];
 			testTypes = [];
-			testDatesLastUpdated = 0;
-			testTypesLastUpdated = 0;
+			testResultsByDate = {};
+			testResultsByType = {};
 		}
 	}
 })();
