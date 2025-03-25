380c40796de311deb1faabc9c64047feeb4600e7
Stacey Beard <staceybeard10@gmail.com>
2025-03-25 10:48:55 -0400

    chore: add copyright notices to new files

--- src/js/constants/branding.constants.js
+++ src/js/constants/branding.constants.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: Copyright (C) 2025 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // For more information on this file, see `docs/setup/branding.md`.
 
 (function () {
