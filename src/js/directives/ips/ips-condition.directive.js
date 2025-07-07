33f9a0319a86a1355a89465e2767985746ccd426
Stacey Beard <staceybeard10@gmail.com>
2025-07-07 16:35:56 -0400

    feat: add more content to directive for IPS condition

--- src/js/directives/ips/ips-condition.directive.js
+++ src/js/directives/ips/ips-condition.directive.js
@@ -22,8 +22,45 @@
             template: `<div class="panel ips-inner-panel">
                            <div class="panel-body">
                                <ips-badge ng-if="statusBadgeText">{{ statusBadgeText }}</ips-badge>
-                               <!-- TODO what if it's undefined? -->
                                <ips-badge use-dynamic-color="true" criticality="resource.severity.text">{{ severityBadgeText }}</ips-badge>
+                               
+                               <div ng-if="resource.category && resource.category[0]">
+                                   <div ng-if="resource.category[0].coding">
+                                       <ips-badge>{{ categoryBadgeText }}</ips-badge>
+
+                                       <div ng-if="resource.category[0].coding[0].display" class="ips-text">
+                                           {{ resource.category[0].coding[0].display }}
+                                       </div>
+                                   </div>
+                                   <div ng-if="resource.category[0].text" class="ips-text">
+                                       <div>{{ resource.category[0].text }}</div>
+                                   </div>
+                               </div>
+
+                               <div ng-if="resource.code">
+                                   <div ng-if="resource.code.coding">
+                                       <ips-badge>{{ codingBadgeText }}</ips-badge>
+
+                                       <div ng-if="resource.code.coding[0].display" class="ips-text">
+                                           {{ resource.code.coding[0].display }}
+                                       </div>
+                                   </div>
+                                   <div ng-if="resource.code.text" class="ips-text">
+                                       <div>{{ resource.code.text }}</div>
+                                   </div>
+                               </div>
+
+                               <div ng-if="site" class="ips-text">
+                                   {{ 'IPS_LABEL_SITE' | translate }} {{ site }}
+                               </div>
+                               
+                               <div ng-if="resource.onsetDateTime" class="ips-text">
+                                   {{ 'IPS_LABEL_SINCE' | translate }} {{ resource.onsetDateTime }}
+                               </div>
+                               
+                               <div ng-if="resource.recordedDate" class="ips-text">
+                                   {{ 'IPS_LABEL_RECORDED' | translate }} {{ resource.recordedDate }}
+                               </div>
                            </div>
                        </div>`,
 
@@ -37,7 +74,10 @@
 
                 // TODO 'unknown'
                 scope.severityBadgeText = `${$filter('translate')('IPS_LABEL_SEVERITY')} ${resource.severity?.text ?? 'unknown'}`;
+                scope.categoryBadgeText = `${resource.category?.[0].coding?.[0].system} : ${resource.category?.[0].coding?.[0].code}`;
+                scope.codingBadgeText = `${resource.code.coding?.[0].system} : ${resource.code.coding?.[0].code}`;
 
+                scope.site = resource.bodySite?.[0]?.coding?.[0]?.display;
             }
         }
     }
