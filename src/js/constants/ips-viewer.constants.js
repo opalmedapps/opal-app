d3d777df1a7942378e311b90602db66c0925740a
Stacey Beard <31229509+staceybeard@users.noreply.github.com>
2025-10-08 10:05:20 -0400

    fix: add CommonHealth viewer iframe to CSP headers for the web app (#1433)
    Added https://viewer.commonhealth.org to the allowed URLs for frames in the web app, to ensure the IPS can be displayed.

--- src/js/constants/ips-viewer.constants.js
+++ src/js/constants/ips-viewer.constants.js
@@ -6,6 +6,8 @@
  * The IPS viewer URL is provided here as a simple export to be available both early in the app's
  * configuration phase (to $sceDelegateProvider) and after configuration (to the IPS controller).
  *
+ * It's also referenced in `static/landingpage/.htaccess` to avoid CSP errors in the web app.
+ *
  * The URL must end in a forward slash.
  */
 export default 'https://viewer.commonhealth.org/';
