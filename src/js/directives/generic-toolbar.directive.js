55d05dd02c6c5db458ba8c78624d5efc73ac6a89
Stacey Beard <staceybeard10@gmail.com>
2026-01-23 15:56:53 -0500

    fix: improve display of content in the ons-toolbar

--- src/js/directives/generic-toolbar.directive.js
+++ src/js/directives/generic-toolbar.directive.js
@@ -9,20 +9,21 @@
         .module('OpalApp')
         .directive("genericToolbar", GenericToolbar);
 
+    GenericToolbar.$inject = ['$timeout'];
+
     /**
      * @name TopToolbar
      * @author David Gagne
      * @date 2021-12-10
      * @desc Directive for the top tool bar with title and back bouton.
      */
-    function GenericToolbar()
+    function GenericToolbar($timeout)
     {
         return {
             restrict: 'E',
             scope: {
                 title: '@',
                 backButton: '=?',
-                styleClass: '=',
             },
             transclude: {
                 'leftContentSlot': '?leftContent',
@@ -30,13 +31,39 @@
             },
             template: `
                 <ons-toolbar fixed-style>
-                    <div class="left" ng-transclude="leftContentSlot" ng-style="iosStyleFix"><ons-back-button ng-hide="backButton === false">{{"BACK"|translate}}</ons-back-button></div>
-                    <div class="center overflow-text-ellipsis" ng-class="styleClass" ng-style="iosStyleFix">{{title}}</div>
-                    <div class="right" ng-transclude="rightContentSlot" ng-style="iosStyleFix"></div>
+                    <div id="toolbar-left-{{$id}}" class="left" ng-transclude="leftContentSlot" ng-style="iosStyleFix">
+                        <ons-back-button id="toolbar-left-default-{{$id}}" ng-hide="backButton === false">{{"BACK"|translate}}</ons-back-button>
+                    </div>
+                    <div id="toolbar-center-{{$id}}" class="center overflow-text-ellipsis" ng-style="iosStyleFix">{{title}}</div>
+                    <div id="toolbar-right-{{$id}}" class="right" ng-transclude="rightContentSlot" ng-style="iosStyleFix"></div>
                 </ons-toolbar>
             `,
             link: function (scope) {
-                scope.iosStyleFix = ons.platform.isIOS() ? {'padding-top': '0px'} : {};
+                $timeout(() => {
+                    // Fix needed to prevent empty space above the toolbar on iOS
+                    scope.iosStyleFix = ons.platform.isIOS() ? {'padding-top': '0px'} : {};
+
+                    // Get references to the toolbar elements
+                    let leftElement = $(`#toolbar-left-${scope.$id}`) || $(`#toolbar-left-default-${scope.$id}`);
+                    let centerElement = $(`#toolbar-center-${scope.$id}`);
+                    let rightElement = $(`#toolbar-right-${scope.$id}`);
+
+                    // Detect whether the toolbar title (center text) was cut off with '...'
+                    // See: https://stackoverflow.com/questions/7738117/html-text-overflow-ellipsis-detection
+                    let ellipsisIsActive = centerElement[0].offsetWidth < centerElement[0].scrollWidth;
+
+                    // Detect whether the right element has content
+                    let rightContentExists = rightElement?.find('right-content').length > 0;
+
+                    // UI optimization: if the title is cut off, but there is nothing in the right slot, use that space to display more of the title
+                    if (ellipsisIsActive && !rightContentExists) {
+                        // Default ons-toolbar space allocation is 27-46-27%
+                        centerElement.css('width', '73%');
+                        centerElement.css('text-align', 'left');
+                        rightElement.css('width', '0%');
+                        rightElement.css('padding-right', '2px');
+                    }
+                });
             }
         };
     }
