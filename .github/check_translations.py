e8cffd3eddb7db67a943f207afea93ca88824269
Matthias Schoettle <git@mattsch.com>
2026-03-26 14:14:09 -0400

    add missing copyright tags

--- .github/check_translations.py
+++ .github/check_translations.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Copyright (C) 2026 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import json
 import sys
 from pathlib import Path
